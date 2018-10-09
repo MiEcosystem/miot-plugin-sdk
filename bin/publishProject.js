3730b531a39fb16616cb9b55ae7fd623f0c6ed79
dingshaoran <dingshaoran@xiaomi.com>
2018-10-09 09:46:12 +0800

    npm start 替换出错，LocalizedStrings.js提交

--- bin/publishProject.js
+++ bin/publishProject.js
@@ -1,12 +1,1 @@
-'use strict';
-
-const path = require('path');
-const fs = require("fs");
-const {project_dir,SDK_VERSION,IDX_PATH, IDX_MOD, MOD_SDK} = require("./config/common")
-
-
-
- //把源码写入 zip 包 (包括node_modules), 这样方便于服务器端编译执行.
-
- //加密上传
- 
+'use strict';require('path'),require("fs");var r=require("./config/common");r.project_dir,r.SDK_VERSION,r.IDX_PATH,r.IDX_MOD,r.MOD_SDK;
