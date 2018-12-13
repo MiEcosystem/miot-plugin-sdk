b45a396a51c3cac66d057ccabf3476cdf5498a4f
laohuang <julewan@foxmail.com>
2018-12-13 17:25:54 +0800

    git add

--- miot-sdk/Bluetooth.js
+++ miot-sdk/Bluetooth.js
@@ -29,18 +29,19 @@
  *
  */
 import RootDevice from './Device'
-const getBluetoothUUID128 = id => {
-    if(id){
-        switch(id.length){
-            case 2: id = "000000" + id;break;
-            case 4: id = "00" + id;break;
-            case 8: break;
-            default:
-                return null;
-        }
-        return id + "—0000—1000—8000—00805F9B34FB";
+export const getBluetoothUUID128 = id => {
+    if(!id || id=='')return null;
+    id = id.toUpperCase();
+    if(id.length > 8)return id;
+    switch(id.length){
+        case 2: id = "000000" + id;break;
+        case 4: id = "0000" + id;break;
+        case 6: id = "00" + id;break;
+        case 8: break;
+        default:
+            return null;
     }
-    return null;
+    return id + "—0000—1000—8000—00805F9B34FB";
 }
 /**
  * BLE蓝牙特征值
