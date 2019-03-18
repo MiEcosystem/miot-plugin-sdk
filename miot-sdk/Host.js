94fbce0c98a7871f350d180ed8e8db449b599984
吴磊 <wulei7@xiaomi.com>
2019-03-18 15:51:41 +0800

    demo 与文档更新

--- miot-sdk/Host.js
+++ miot-sdk/Host.js
@@ -193,7 +193,7 @@ export default {
      * 后台执行文件, 后台最多同时运行三个线程, 超过将销毁最早创建的 executor
      * @since 10002
      * @param {*} jx - 可执行的纯 js 文件, 不使用任何高级语法, 如要使用 es6, 请自行编译通过.
-     * @param {json} initialProps - 用于脚本初始化的数据
+     * @param {json} initialProps - 用于脚本初始化的数据, 在jx文件中为 'initialProps' 对象，使用方法参考样例 或者sampleProject中 ‘com.xiaomi.demo/Main/tutorial/JSExecutor.js’
      * @returns {Promise<IExecutor>} 
      * @example
      * 
@@ -205,6 +205,10 @@ export default {
      *                  .then(result=>{
      *                      console.log(result);
      *                  })
+     *          //支持使用initialProps或者在jx中直接使用
+     *          executor.execute("myFunc2", "initialProps.name1").then(res =>{...})
+     *          //支持使用obj与arr
+     *          executor.execute("SomeObject.myFunc3", {"name":"hello"}, ["a1","a2"]).then(res =>{...})
      * })
      * .then(err=>{...})
      * ....
--- projects/com.xiaomi.demo/Main/tutorial/JSExecutor.js
+++ projects/com.xiaomi.demo/Main/tutorial/JSExecutor.js
@@ -23,7 +23,33 @@ export default class JSExecutor extends React.Component {
         this.executor&&this.executor.remove();
     }
 
-    call3() {
+    ObjectMethodCall() {
+        if (this.executor) {
+            //支持使用Obj对象中的method
+            this.executor.execute("TestObj.callWithArg1ReturnNumber", "hello world").then(res => {
+                console.log("ObjectMethodCall result :", res);
+                alert("success: " + JSON.stringify(res));
+            }).catch(err => {
+                console.log("ObjectMethodCall failed :", err);
+                alert("failed: ", JSON.stringify(err));
+            })
+        }
+    }
+
+    callWithObject() {
+        if (this.executor) {
+            //execute参数支持任意可以json序列化的对象
+            this.executor.execute("callWithObj", "initialProps.init1", ['array1', 'array2'], {'map1':'map1'}).then(res => {
+                console.log("callWithObj result :", res);
+                alert("success: " + JSON.stringify(res));
+            }).catch(err => {
+                console.log("callWithObj failed :", err);
+                alert("failed: ", JSON.stringify(err));
+            })
+        }
+    }
+
+    call3Params() {
         if (this.executor) {
             this.executor.execute("callWithArg3ReturnOBJ", "1", "2", "3").then(res => {
                 console.log("call 3 result :", res);
@@ -39,7 +65,9 @@ export default class JSExecutor extends React.Component {
         return (
             <View>
                 <Text>JSExecutor</Text>
-                <Button onPress={this.call3.bind(this)} title="函数调用"></Button>
+                <Button onPress={this.call3Params.bind(this)} title="简单函数调用"></Button>
+                <Button onPress={this.callWithObject.bind(this)} title="函数调用传递对象"></Button>
+                <Button onPress={this.ObjectMethodCall.bind(this)} title="Obj方法调用"></Button>
             </View>
         )
     }
