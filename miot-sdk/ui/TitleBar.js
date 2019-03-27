50381a757bf401197c979f285ef7f3b120268d9b
lvjie1 <lvjie1@xiaomi.com>
2019-03-27 14:08:35 +0800

    修复 TitleBar 适配Android

--- miot-sdk/ui/TitleBar.js
+++ miot-sdk/ui/TitleBar.js
@@ -1,204 +1,194 @@
 /**
  * @export
  * @module miot/ui/TitleBar
- * @author Geeook
  * @description 新版导航栏，可以尝试使用
  * @property type 导航栏类型 options: ["dark", "light"(default)]
  * @property style 导航栏整体的样式
+ * @property leftTextStyle 左侧文字样式，和 leftText 一起使用，不设置使用米家默认值
  * @property leftText 左侧文字
- * @property onPressLeft 左侧第一个按钮的点击事件，设置该属性将显示默认的「返回」按钮，否则不显示
- * @property onPressLeft2 左侧第二个按钮的点击事件，设置 leftText 则显示设置的文字，否则显示默认的「关闭」按钮。
+ * @property onPressLeft 左侧点击事件，设置了才显示左侧文字或图片，如果设置了leftText则显示设置的文字，否则显示默认的返回按钮。
+ * @property onPressLeft2 左侧的第二个点击事件，设置了才显示默认的关闭按钮，
+ * @property rightTextStyle 右侧文字样式，和 rightText 一起使用，不设置使用米家默认值
  * @property rightText 右侧文字
- * @property onPressRight 右侧第一个按钮的点击事件，设置该属性将显示默认的「分享」按钮，否则不显示。
- * @property onPressRight2 右侧第二个按钮的点击事件，设置 rightText 则显示设置的文字，否则显示默认的「更多」按钮。
- * @property title 中间的标题文字
- * @property subTitle  中间的副标题文字
- * @property onPressTitle 点击标题 / 副标题的事件
- * @property showDot 是否显示右侧「更多」按钮的小红点
- * @property flex 标题部分占导航栏的占比，默认1: 1.5: 1
+ * @property onPressRight 右侧点击事件，设置了才显示右侧文字或图片，如果设置了 rightText 则显示设置的文字，否则显示默认的更多按钮。
+ * @property onPressRight2 右侧的第二个点击事件，设置了才显示默认的分享按钮
+ * @property title 中间的标题
+ * @property subTitle  中间的子标题
+ * @property onPressTitle 点击标题的事件
+ * @property showDot 是否显示右侧更多按钮的空点
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Dimensions,
-  StatusBar,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    StatusBar,
+    Platform,
 } from 'react-native';
 import ImageButton from './ImageButton';
-import { SafeAreaView } from 'react-navigation';
-const { width } = Dimensions.get('window');
-const titleHeight = 48;
+import {SafeAreaView} from 'react-navigation';
+import {RkButton} from "react-native-ui-kitten";
+const PropTypes = require('prop-types');
+const {width, height} = Dimensions.get('window');
+const titleHeight = 44;
 const imgHeight = 28;
-const marginBetWeenImgs = (titleHeight - imgHeight) / 2;
-const marginBetWeenImgText = 3;
 export default class TitleBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    let dark = this.props.type === 'dark';
-    dark ? StatusBar.setBarStyle('light-content') : StatusBar.setBarStyle('default');
-    return (
-      <SafeAreaView style={[styles.titleBarContainer, this.props.style, dark ? { backgroundColor: '#333' } : {}]}>
-        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', paddingLeft: marginBetWeenImgs }}>
-          {this.props.onPressLeft &&
-            <ImageButton
-              onPress={this.props.onPressLeft}
-              style={[styles.img]}
-              source={
-                dark ?
-                  require('../resources/title/back_white_normal.png') :
-                  require('../resources/title/back_normal.png')
-              }
-              highlightedSource={
-                dark ?
-                  require('../resources/title/back_white_press.png') :
-                  require('../resources/title/back_press.png')
-              }
-            />
-          }
-          {this.props.leftText ?
-            <Text
-              style={[
-                styles.leftRightText,
-                this.props.onPressLeft ? { marginLeft: marginBetWeenImgText } : {},
-                dark ? { color: '#fff' } : {}
-              ]}
-              onPress={this.props.onPressLeft2 || this.props.onPressLeft || (_ => { console.warn('no callback for leftText!') })}
-            >
-              {this.props.leftText}
-            </Text> :
-            (this.props.onPressLeft2 &&
-              <ImageButton
-                onPress={this.props.onPressLeft2}
-                style={[styles.img, { marginLeft: marginBetWeenImgs, }]}
-                source={
-                  dark ?
-                    require('../resources/title/close_white_normal.png') :
-                    require('../resources/title/close_normal.png')
+    static propTypes = {
+        type: PropTypes.oneOf(['dark', 'light']),
+    }
+    static defaultProps = {
+        type:'light',
+    }
+    constructor(props) {
+        super(props);
+    }
+    componentWillMount() {
+        this.isDarkStyle = this.props.type === 'dark';
+        this.isDarkStyle ? StatusBar.setBarStyle('dark-content') : StatusBar.setBarStyle('light-content');
+        console.log(this.props.type, this.isDarkStyle)
+        if (Platform.OS == 'android') {
+            StatusBar.setTranslucent(true);
+        }
+    }
+    render() {
+        let leftWidth = this.props.leftTextStyle ? this.props.leftTextStyle.width : 0;
+        let rightWidth = this.props.rightTextStyle ? this.props.rightTextStyle.width : 0;
+        let titleTextStyle = this.isDarkStyle?styles.blackTitleText:styles.whiteTitleText;
+        let subtitleTextStyle = this.isDarkStyle?styles.blackSubtitleText:styles.whiteSubtitleText;
+        let leftRightTextStyle = this.isDarkStyle?styles.blackLeftRightText:styles.whiteLeftRightText;
+        return (
+            <SafeAreaView style={[styles.titleBarContainer, this.props.style]}>
+                {this.props.leftText
+                    ? <RkButton onPress={this.props.onPressLeft}
+                                contentStyle={[leftRightTextStyle, this.props.leftTextStyle]}
+                                style={[leftRightTextStyle, {
+                                    height: this.props.onPressLeft ? titleHeight : 0,
+                                    width: leftWidth ? leftWidth : imgHeight + 14 * 2
+                                }]}>{this.props.leftText}</RkButton>
+                    : <ImageButton onPress={this.props.onPressLeft}
+                                   style={[styles.img, {height: this.props.onPressLeft ? imgHeight : 0}]}
+                                   source={require('../resources/title/std_tittlebar_main_device_back_normal.png')}
+                                   highlightedSource={require('../resources/title/std_tittlebar_main_device_back_press.png')}/>
                 }
-                highlightedSource={
-                  dark ?
-                    require('../resources/title/close_white_press.png') :
-                    require('../resources/title/close_press.png')
+                <ImageButton onPress={this.props.onPressLeft2}
+                             style={[styles.img, {
+                                 marginLeft: 0,
+                                 height: this.props.onPressLeft2 ? imgHeight : 0,
+                             }]}
+                             source={require('../resources/title/std_tittlebar_main_device_back2_normal.png')}
+                             highlightedSource={require('../resources/title/std_tittlebar_main_device_back2_press.png')}/>
+                <View style={[styles.textContainer]}>
+                    <Text
+                        style={[titleTextStyle]}
+                        onPress={this.props.onPressTitle}>{this.props.title}</Text>
+                    {
+                        this.props.subTitle && <Text
+                            style={[subtitleTextStyle]}
+                            onPress={this.props.onPressTitle}>{this.props.subTitle}</Text>
+                    }
+                </View>
+                <ImageButton onPress={this.props.onPressRight2}
+                             style={[styles.img, {
+                                 marginRight: 0,
+                                 height: this.props.onPressRight2 ? imgHeight : 0,
+                             }]}
+                             source={require('../resources/title/std_tittlebar_main_device_share_normal.png')}
+                             highlightedSource={require('../resources/title/std_tittlebar_main_device_share_press.png')}/>
+                {this.props.rightText
+                    ? <RkButton onPress={this.props.onPressRight}
+                                contentStyle={[leftRightTextStyle, this.props.rightTextStyle]}
+                                style={[leftRightTextStyle, {
+                                    height: this.props.onPressRight ? titleHeight : 0,
+                                    width: rightWidth ? rightWidth : imgHeight + 14 * 2
+                                }]}>{this.props.rightText}</RkButton>
+                    : <ImageButton onPress={this.props.onPressRight}
+                                   style={[styles.img, {height: this.props.onPressRight ? imgHeight : 0}]}
+                                   source={require('../resources/title/std_tittlebar_main_device_more_normal.png')}
+                                   highlightedSource={require('../resources/title/std_tittlebar_main_device_more_press.png')}/>
                 }
-              />
-            )
-          }
-        </View>
-        <View style={{ flex: this.props.flex || 1.5, justifyContent: 'center', alignItems: 'center' }}>
-          <Text
-            numberOfLines={1}
-            ellipsizeMode='tail'
-            style={[styles.titleText, dark ? { color: '#fff' } : {}]}
-            onPress={this.props.onPressTitle}>
-            {this.props.title}
-          </Text>
-          {this.props.subTitle &&
-            <Text
-              numberOfLines={1}
-              ellipsizeMode='tail'
-              style={[styles.subtitleText, dark ? { color: '#fff' } : {}]}
-              onPress={this.props.onPressTitle}>
-              {this.props.subTitle}
-            </Text>
-          }
-        </View>
-        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', paddingRight: marginBetWeenImgs }}>
-          <View style={{ flex: 1 }} />
-          {this.props.onPressRight &&
-            <ImageButton
-              onPress={this.props.onPressRight}
-              style={[styles.img]}
-              source={
-                dark ?
-                  require('../resources/title/share_white_normal.png') :
-                  require('../resources/title/share_normal.png')
-              }
-              highlightedSource={
-                dark ?
-                  require('../resources/title/share_white_press.png') :
-                  require('../resources/title/share_press.png')
-              }
-            />
-          }
-          {this.props.rightText ?
-            <Text
-              style={[
-                styles.leftRightText,
-                { marginLeft: marginBetWeenImgText },
-                dark ? { color: '#fff' } : {}
-              ]}
-              onPress={this.props.onPressRight2 || this.props.onPressRight || (_ => { console.warn('no callback for rightText!') })}
-            >
-              {this.props.rightText}
-            </Text> :
-            (this.props.onPressRight2 &&
-              <View style={[styles.img, { marginLeft: marginBetWeenImgs }]}>
-                <ImageButton
-                  onPress={this.props.onPressRight2}
-                  style={[styles.img]}
-                  source={
-                    dark ?
-                      require('../resources/title/more_white_normal.png') :
-                      require('../resources/title/more_normal.png')
-                  }
-                  highlightedSource={
-                    dark ?
-                      require('../resources/title/more_white_press.png') :
-                      require('../resources/title/more_press.png')
-                  }
-                />
-                {this.props.showDot &&
-                  <Image
-                    style={styles.dot}
-                    source={require('../resources/title/dot.png')}
-                  />
+                {
+                    this.props.showDot &&
+                    <Image style={styles.dot}
+                           source={require('../resources/title/std_tittlebar_main_device_massage_point.png')}/>
                 }
-              </View>
-            )
-          }
-        </View>
-      </SafeAreaView>
-    );
-  }
+            </SafeAreaView>
+        );
+    }
 }
 const styles = StyleSheet.create({
-  titleBarContainer: {
-    width: width,
-    height: titleHeight,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-  },
-  titleText: {
-    fontSize: 17,
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-  subtitleText: {
-    marginTop: 3,
-    color: '#666',
-    fontSize: 10,
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-  leftRightText: {
-    color: '#666',
-    fontSize: 14,
-    textAlignVertical: "center",
-    textAlign: "center"
-  },
-  img: {
-    width: imgHeight,
-    height: imgHeight,
-  },
-  dot: {
-    position: 'absolute',
-    width: 8,
-    height: 8,
-    resizeMode: 'contain',
-    right: 0,
-  },
+    titleBarContainer: {
+        flexDirection: 'row',
+        width: width,
+        alignItems: 'flex-end',
+        height: StatusBar.currentHeight + titleHeight,
+    },
+    textContainer: {
+        height: titleHeight,
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    blackTitleText: {
+        color: '#000000cc',
+        fontSize: 15,
+        textAlignVertical: 'center',
+        textAlign: 'center',
+    },
+    whiteTitleText: {
+        color: "#ffffffcc",
+        fontSize: 15,
+        textAlignVertical: "center",
+        textAlign: "center"
+    },
+    blackSubtitleText: {
+        color: '#00000088',
+        fontSize: 12,
+        textAlignVertical: 'center',
+        textAlign: 'center',
+    },
+    whiteSubtitleText: {
+        color: "#ffffff88",
+        fontSize: 12,
+        textAlignVertical: "center",
+        textAlign: "center"
+    },
+    blackLeftRightText: {
+        flexDirection: 'column',
+        backgroundColor: '#0000',
+        color: '#00000088',
+        fontSize: 14,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlignVertical: "center",
+        textAlign: "center"
+    },
+    whiteLeftRightText: {
+        flexDirection: 'column',
+        backgroundColor:'#0000',
+        color: '#ffffff88',
+        fontSize: 14,
+        alignItems:'center',
+        justifyContent:'center',
+        textAlignVertical: "center",
+        textAlign: "center"
+    },
+    img: {
+        width: imgHeight,
+        height: imgHeight,
+        resizeMode: 'contain',
+        marginLeft: 14,
+        marginTop: (titleHeight - 28) / 2,
+        marginBottom: (titleHeight - 28) / 2,
+        marginRight: 14,
+    },
+    dot: {
+        position: 'absolute',
+        width: 10,
+        height: 10,
+        resizeMode: 'contain',
+        right: 14,
+        top: StatusBar.currentHeight + (titleHeight - 28) / 2,
+    },
 });
