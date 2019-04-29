0f3cf21b700f5f1441e320830cdc0b897c2d974b
吴磊 <869938879@qq.com>
2019-04-29 15:53:58 +0800

    feat: demo update  refer 91c74f7eb012f4496c5bed78e8eb4c63a753f334

--- projects/com.xiaomi.demo/index.js
+++ projects/com.xiaomi.demo/index.js
@@ -1,21 +1,22 @@
+import { Device, Entrance, Host, Package, PackageEvent } from "miot";
 import App from "./Main";
-import {Package, Entrance, PackageEvent, Host, Device} from "miot";
-PackageEvent.packageAuthorizationCancel.addListener(()=>{
+import Scene from './Main/SceneMain';
+PackageEvent.packageAuthorizationCancel.addListener(() => {
     // 用户撤销授权,需要清除缓存
     console.log("packageAuthorizationCancel");
-    let licenseKey = "license-"+Device.deviceID;
+    let licenseKey = "license-" + Device.deviceID;
     Host.storage.set(licenseKey, false);
     Package.exit();
 })
-PackageEvent.packageViewWillAppear.addListener(()=>{console.log("packageViewWillAppear")});
-PackageEvent.packageWillLoad.addListener(()=>{console.log("packageWillLoad")});
-PackageEvent.packageDidLoaded.addListener(()=>{console.log("packageDidLoaded")});
-PackageEvent.packageDidResume.addListener(()=>{console.log("packageDidResume")});
-PackageEvent.packageWillPause.addListener(()=>{console.log("packageWillPause")});
-PackageEvent.packageWillExit.addListener(()=>{console.log("packageWillExit")});
+PackageEvent.packageViewWillAppear.addListener(() => { console.log("packageViewWillAppear") });
+PackageEvent.packageWillLoad.addListener(() => { console.log("packageWillLoad") });
+PackageEvent.packageDidLoaded.addListener(() => { console.log("packageDidLoaded") });
+PackageEvent.packageDidResume.addListener(() => { console.log("packageDidResume") });
+PackageEvent.packageWillPause.addListener(() => { console.log("packageWillPause") });
+PackageEvent.packageWillExit.addListener(() => { console.log("packageWillExit") });
 switch (Package.entrance) {
     case Entrance.Scene:
-        Package.entry(require('./Main/SceneMain'), _ => {
+        Package.entry(Scene, _ => {
         });
         break;
     default:
