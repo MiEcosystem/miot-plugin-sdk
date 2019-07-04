46656d95ca55663550133930b406acc6c894d30b
吴磊 <869938879@qq.com>
2019-07-05 00:07:44 +0800

    Merge branch feat_10023_lumi_scene into SDK_10023
    update desc
    
    See merge request !66 refer da066ad920750b7e52d20c45d4a7bec50197d70f

--- miot-sdk/host/crypto.js
+++ miot-sdk/host/crypto.js
@@ -88,4 +88,23 @@ export default {
   pointsToImageBase64(width, height, points, colorsMap) {
      return Promise.resolve('');
   },
+  /**
+   * ApiLevel: 10023
+   * @since 10023
+   * 2019.05.16  针对第三方要求新增的接口
+   * 扫地机的地图转换
+   * 根据点集合长宽以及每个点对应的颜色值生成bitmap并返回其base64字符串
+   * @param width : 图片宽度
+   * @param height : 图片高度
+   * @param points : 点集合字符串
+   * @param colorsMap : 点值与颜色之间对应关系JSON字符串
+   * @param scale : 缩放比例
+   * -1 墙 #666666
+   * 0 背景 #E6EAEE
+   * 1 发现区域 #C6D8FA
+   * >=10 房间区域
+   */
+  pointsScaleToImageBase64(width, height, points, colorsMap, scale) {
+     return Promise.resolve('');
+  }
 };
