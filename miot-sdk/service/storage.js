d1518f2dc9e3c14a0d2e4119e9200bba2e88fc98
吴磊 <869938879@qq.com>
2019-07-13 15:43:04 +0800

    Merge remote-tracking branch origin/SDK_10023 into SDK_10023  refer 854d147b59f73bdf52323a0018d161f97639bbd3

--- miot-sdk/service/storage.js
+++ miot-sdk/service/storage.js
@@ -22,6 +22,7 @@ export default {
      * 读取三方数据,该接口读取厂商的用户配置信息 /user/get_third_user_config，对应的写的接口为：set_third_user_config。
      * @param {string} model 设备Model
      * @param  {...number} keys 根据key获取配置,如果不传keys 返回用户该厂商的所有配置
+     * @deprecated 10023开始废弃，建议使用gethirdUserConfigsForOneKey, 与sethirdUserConfigsForOneKey配套使用
      * @example
      * getThirdUserConfigs(model, k1,k2,k3).then(res => {...})
      */
@@ -38,10 +39,11 @@ export default {
          return Promise.resolve(null);
     },
     /**
-     * 写数据 /user/set_user_config
+     * 写数据 /user/set_user_config， data的数据量不能超过2048字节
      * @param {string} model 
      * @param {number} key 
-     * @param {json} data 
+     * @param {json} data
+     * @deprecated 10023开始废弃，建议使用setThirdUserConfigsForOneKey, data数据量支持分段保存
      */
     setThirdUserConfigs(model, key, data) {
          return Promise.resolve(null);
@@ -51,6 +53,7 @@ export default {
      * 创建或修改设置插件自由存储空间。如果数据超过服务器设置的阈值，自动分段存储到云端。
      * 但是分段存储会占用额外的key，比如key=100时，分出的新段会存储在101,102,103...等后续相邻的key上，
      * 因此如果插件方需要存储多个key-value，建议多个key之间相差较大
+     * @since 10023
      * @param {string} model
      * @param {number} key
      * @param {json} data
@@ -60,6 +63,7 @@ export default {
     },
     /**
      * 与setThirdUserConfigsForOneKey配套使用，会把分段的数据自动合并后返回，使得分段行为对调用者透明
+     * @since 10023
      * @param model
      * @param key
      * @returns {Promise<any>}
