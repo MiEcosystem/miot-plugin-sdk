ed78fcb221e7e6873b7f810c5d771f9a15fea632
santcool <santcool@126.com>
2019-08-19 15:02:39 +0800

    fix android callMethodFromLocal  refer e26c04e29148ca7723e75b100b08f2e0cb8c518a

--- miot-sdk/Device.js
+++ miot-sdk/Device.js
@@ -281,7 +281,7 @@ export class IDeviceWifi {
      * })
      * .catch(() => console.log('subscribe failed'))
      * ...
-     * 
+     *
      * ...
      * //unsubscribe the props
      * msgSubscription&&msgSubscription.remove();
@@ -490,17 +490,17 @@ class IDevice {
          return Promise.resolve([]);
     }
     /**
-     * 获取小米BLE蓝牙控制类, 
-     * 注意: 在 iOS 平台上, 如果没有指定peripheralID, 则须先执行Bluetooth.scan(), 
+     * 获取小米BLE蓝牙控制类,
+     * 注意: 在 iOS 平台上, 如果没有指定peripheralID, 则须先执行Bluetooth.scan(),
      * 扫描到与device.mac匹配的蓝牙设备之后才能 connect 成功, 否则将不能成功执行后来的所有操作.
      * @method
      * @param {string} peripheralID -- iOS平台上可以直接指定与设备 mac 匹配的peripheralID, android 平台不需要此参数
      * @returns {IBluetoothLE}
      * @see {@link module:miot/Bluetooth}
      * @example
-     * 
+     *
      * const peripheralUUID4IOS = ...;
-     * 
+     *
      * Device.getBluetoothLE(peripheralUUID4IOS).connect()
      * .then(ble=>{
      *      ble....
@@ -508,7 +508,7 @@ class IDevice {
      * .catch(error=>{
      *
      * })
-     * 
+     *
      *
      */
     getBluetoothLE(peripheralID = null) {
@@ -915,7 +915,7 @@ class IDevice {
      * 不支持单模蓝牙、组设备、虚拟设备、离线设备、分享设备。
      * @returns {Promise}
      * @example
-     * 
+     *
      * //首先屏蔽默认弹窗
      * Package.disableAutoCheckUpgrade = true;
      * //....
