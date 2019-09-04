927fdfb1c8ddaae25b6cc94d1196e01544b0308f
santcool <zhangwenfeng@xiaomi.com>
2019-09-04 14:59:12 +0800

    feat: update setting  refer 90ef63a0576d9cf39ae82b09cc28de1e9c01951d

--- projects/com.xiaomi.demo/Main/tutorial/Setting.js
+++ projects/com.xiaomi.demo/Main/tutorial/Setting.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { Entrance, Package } from "miot";
 import { strings, Styles } from 'miot/resources';
 import { CommonSetting, SETTING_KEYS } from "miot/ui/CommonSetting";
 import { firstAllOptions, secondAllOptions } from "miot/ui/CommonSetting/CommonSetting";
@@ -8,7 +9,6 @@ import Separator from 'miot/ui/Separator';
 import TitleBar from 'miot/ui/TitleBar';
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import {Entrance, Package} from "miot";
 
 const { first_options, second_options } = SETTING_KEYS;
 
@@ -21,14 +21,14 @@ export default class Setting extends React.Component {
           type='dark'
           title={strings.setting}
           style={{ backgroundColor: '#fff' }}
-          onPressLeft={()=>{
-              if(Package.entrance !== Entrance.Main && !Package.pageParams.isBackToMainPage){
-                // 如果是通过Host.ui.openPluginPage 跳转到此页面，且不需要返回到插件首页，则直接调用退出插件api
-                Package.exit();
-              }else{
-                navigation.goBack();
-              }
+          onPressLeft={() => {
+            if (Package.entrance !== Entrance.Main && !Package.pageParams.isBackToMainPage) {
+              // 如果是通过Host.ui.openPluginPage 跳转到此页面，且不需要返回到插件首页，则直接调用退出插件api
+              Package.exit();
+            } else {
+              navigation.goBack();
             }
+          }
           }
         />
     };
@@ -74,6 +74,7 @@ export default class Setting extends React.Component {
         hideAgreement: true
       },
       syncDevice: true,
+      // networkInfoConfig: -1,
     }
     return (
       <View style={styles.container}>
