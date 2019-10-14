a0b821a2ea2ab7c355e717aa7901d49f772af011
santcool <zhangwenfeng@xiaomi.com>
2019-10-14 10:56:52 +0800

    修改demo， 验证StringSpinner 滑动冲突  refer 1fab0b00b21cd07732ac351ad7f965c16193f00e

--- projects/com.xiaomi.demo/Main/UIComponent/StringSpinnerDemo.js
+++ projects/com.xiaomi.demo/Main/UIComponent/StringSpinnerDemo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {
     View,
-    Text,
+    Text,ScrollView
 } from 'react-native';
 
 import { StringSpinner } from 'miot/ui'
@@ -21,25 +21,44 @@ export default class StringSpinnerDemo extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, marginTop: 40, marginLeft: 15, backgroundColor: "#f8f8f8" }}>
+
+              <ScrollView
+                ref={'myScrollView'}
+                bounces={true}
+              >
+
                 <View style={{ flexDirection: 'row' }}>
 
-                    <StringSpinner
-                        style={{ width: 120, height: 200, backgroundColor: '#ffffff', }}
-                        dataSource={['a', 'b', 'c', 'd', 'e', 'f', 'g']}
-                        defaultValue={'c'}
-                        onValueChanged={(data) => { this.updateOneValue(data) }}
-                    />
+                  <StringSpinner
+                    style={{ width: 120, height: 200, backgroundColor: '#ffffff', }}
+                    dataSource={['a', 'b', 'c', 'd', 'e', 'f', 'g']}
+                    defaultValue={'c'}
+                    onValueChanged={(data) => { this.updateOneValue(data) }}
+                  />
 
-                    <StringSpinner
-                        style={{ width: 120, height: 200, marginLeft: 10, backgroundColor: '#ffffff', }}
-                        dataSource={['a', 'b', 'c', 'd', 'e', 'f', 'g']}
-                        defaultValue={'c'}
-                        unit={"斤"}
-                        pickerInnerStyle={{ lineColor: "#ff0000", textColor: "#ff0000", selectTextColor: "#00ff00", fontSize: 14, selectFontSize: 18, rowHeight: 54, selectBgColor: "#eeeeee", unitFontSize: 12, unitTextColor: '#f7632a' }}
-                        onValueChanged={(data) => { this.updateOneValue(data) }}
-                    />
+                  <StringSpinner
+                    style={{ width: 120, height: 200, marginLeft: 10, backgroundColor: '#ffffff', }}
+                    dataSource={['a', 'b', 'c', 'd', 'e', 'f', 'g']}
+                    defaultValue={'c'}
+                    unit={"斤"}
+                    pickerInnerStyle={{ lineColor: "#ff0000", textColor: "#ff0000", selectTextColor: "#00ff00", fontSize: 14, selectFontSize: 18, rowHeight: 54, selectBgColor: "#eeeeee", unitFontSize: 12, unitTextColor: '#f7632a' }}
+                    onValueChanged={(data) => { this.updateOneValue(data) }}
+                  />
                 </View>
                 <Text style={{ marginTop: 20, color: "#666", fontSize: 14 }}>{"选中的值为：" + this.state.oneValue}</Text>
+
+                <View style={{height: 200, backgroundColor: '#999'}}/>
+                <View style={{height: 200, backgroundColor: '#a12'}}/>
+                <View style={{height: 200, backgroundColor: '#cda'}}/>
+                <View style={{height: 200, backgroundColor: '#999'}}/>
+                <View style={{height: 200, backgroundColor: '#1ac'}}/>
+                <View style={{height: 200, backgroundColor: '#5aa'}}/>
+                <View style={{height: 200, backgroundColor: '#8b2'}}/>
+                <View style={{height: 200, backgroundColor: '#cb2'}}/>
+                <View style={{height: 200, backgroundColor: '#1b2'}}/>
+
+
+              </ScrollView>
             </View >
         )
     }
