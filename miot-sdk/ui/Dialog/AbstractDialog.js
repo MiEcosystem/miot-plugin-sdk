9a99a6f11f0091e4c6c9ae0e7ac9e0d827e36263
santcool <zhangwenfeng@xiaomi.com>
2019-11-20 11:51:22 +0800

    fix: add fontFamily as empty string only for android in AbstractDialog  refer 4c4f2725a70eb648aa4a60cc5b3fbd7261976a7a

--- miot-sdk/ui/Dialog/AbstractDialog.js
+++ miot-sdk/ui/Dialog/AbstractDialog.js
@@ -1,7 +1,7 @@
 import { Host } from 'miot';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Dimensions, Modal, StyleSheet, Text, TouchableHighlight, TouchableWithoutFeedback, View } from 'react-native';
+import { Dimensions, Modal, Platform, StyleSheet, Text, TouchableHighlight, TouchableWithoutFeedback, View } from 'react-native';
 import { strings, Styles } from '../../resources';
 import Separator from '../Separator';
 const { width, height } = Dimensions.get('window');
@@ -92,6 +92,12 @@ export default class AbstractDialog extends React.Component {
             titleLines = 3;
             height.maxHeight = 86;
         }
+        //只给安卓手机设置字体为空字符串
+        let fontFamily = {};
+        if (Platform.OS === 'android') {
+            //Android 设备或模拟器
+            fontFamily.fontFamily = '';
+        }
         return (
             <View style={[styles.titleContainer, height]}>
                 <Text
@@ -102,10 +108,10 @@ export default class AbstractDialog extends React.Component {
                             textAlign: 'center',
                             fontSize: 15,
                             fontWeight: 'bold',
-                            fontFamily: '',
                             color: '#000'
                         },
-                        marginBottom
+                        marginBottom,
+                        fontFamily
                     ]}
                 >
                     {this.props.title || ''}
