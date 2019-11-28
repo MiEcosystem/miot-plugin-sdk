2f7237ae6eaeaef304129e8064654b5f4df5b5a2
santcool <zhangwenfeng@xiaomi.com>
2019-11-28 12:51:06 +0800

    Merge branch xb-plugin-fan into SDK_10031
    Xb plugin fan
    
    See merge request !137 refer 259037203315b31c1f6de37ec5c8e36906b31a74

--- miot-sdk/ui/CircleButton.js
+++ miot-sdk/ui/CircleButton.js
@@ -22,7 +22,11 @@ export default class CircleButton extends Component {
     onPress: PropTypes.func,
     title: PropTypes.string,
     icon: PropTypes.any,
-    iconSelected: PropTypes.any
+    iconSelected: PropTypes.any,
+    iconText: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string
+    ])
   };
   static defaultProps = {
     sizeLevel: 0,
@@ -33,7 +37,8 @@ export default class CircleButton extends Component {
     onPress: NOOP,
     title: '',
     icon: null,
-    iconSelected: null
+    iconSelected: null,
+    iconText: ''
   };
   onPress = () => {
     let {disabled, onPress} = this.props;
@@ -43,7 +48,7 @@ export default class CircleButton extends Component {
     onPress();
   }
   render() {
-    let {sizeLevel, selected, title, icon, iconSelected, themeColor, disabled, horizontal} = this.props;
+    let {sizeLevel, selected, title, icon, iconSelected, iconText, themeColor, disabled, horizontal} = this.props;
     let containerSizeStyle = Styles[['container0', 'container1', 'container2', 'container3'][sizeLevel || 0]] || Styles.container0;
     let iconContainerSizeStyle = Styles[['iconContainer0', 'iconContainer1', 'iconContainer2', 'iconContainer3'][sizeLevel || 0]] || Styles.iconContainer0;
     let iconSizeStyle = Styles[['icon0', 'icon1', 'icon2', 'icon3'][sizeLevel || 0]] || Styles.icon0;
@@ -54,7 +59,11 @@ export default class CircleButton extends Component {
           backgroundColor: themeColor || ColorGreen,
           borderColor: themeColor || ColorGreen
         } : null, disabled ? Styles.iconContainerDisabled : null, disabled && selected ? Styles.iconContainerDisabledSelected : null])} activeOpacity={1} onPress={this.onPress}>
-          <Image style={StyleSheet.flatten([Styles.icon, iconSizeStyle])} source={selected && !disabled ? (iconSelected || iconSelected) : icon} />
+          {icon ? (
+            <Image style={StyleSheet.flatten([Styles.icon, iconSizeStyle])} source={selected && !disabled ? (iconSelected || iconSelected) : icon} />
+          ) : (
+            <Text style={[Styles.iconText, selected && !disabled ? Styles.iconTextSelected : null]}>{iconText}</Text>
+          )}
         </TouchableOpacity>
         {title ? (
           <Text style={StyleSheet.flatten([Styles.title, selected ? {
@@ -136,6 +145,14 @@ const Styles = StyleSheet.create({
   },
   icon0: {
     width: Size60
+  },
+  iconText: {
+    fontFamily: FontLantingLight,
+    fontSize: adjustSize(36),
+    color: '#000'
+  },
+  iconTextSelected: {
+    color: '#FFF'
   },
   title: {
     marginTop: adjustSize(27),
