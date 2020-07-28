0aec1c9975bfc4fb9ad66ed9bc64ac34d9a4c9fa
mihome_builder <builder@TonydeMac-Pro.local>
2020-07-28 14:41:21 +0800

    add v2/recipes/query refer none

--- miot-sdk/service/apiRepo.js
+++ miot-sdk/service/apiRepo.js
@@ -161,8 +161,8 @@ const API_REPO = {
   '/v2/device/get_break_lock_alarm_info': {},
   '/v2/device/set_hijack_alarm_info': {},
   '/v2/device/get_hijack_alarm_info': {},
-  '/v2/device/del_alarm_info': {},
-  '/v2/home/get_interim_file_url_pro':{},
-  '/home/getfileurl_v3':{}
+  '/v2/home/get_interim_file_url_pro': {},
+  '/home/getfileurl_v3': {},
+  '/v2/recipes/query': {}
 };
 export default API_REPO;
