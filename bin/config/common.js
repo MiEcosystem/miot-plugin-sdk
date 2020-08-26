665e6e371233fa2ba008ac6514943b9940369c0a
miot-robot <miot-robot@xiaomi.com>
2020-08-26 15:58:23 +0800

    Merge branch revert-895834e1 into SDK_10043 refer 952094a7939117917fab5bf71b25a2588512f1c3

--- bin/config/common.js
+++ bin/config/common.js
@@ -62,15 +62,14 @@ function objectWithoutProperties(obj, ...keys) {
 function exec(command, args=[], onFinish=null, ignore_out=false){
     const cmd = spawn(command, args);
     cmd.stdout.on('data', (data) => {
-      ignore_out || console.log(`[PID ${ cmd.pid }] ${ data.toString() }`);
+      ignore_out || console.log(data.toString());
     });
     cmd.stderr.on('data', (data) => {
-      ignore_out || console.log(`[PID ${ cmd.pid }] error：${data}`); 
+      ignore_out || console.log(`error：${data}`); 
     });
     cmd.on('close', (code) => { 
       onFinish&&onFinish(code); 
     });
-    return cmd;
 }
 
 function absolutePath(p){
