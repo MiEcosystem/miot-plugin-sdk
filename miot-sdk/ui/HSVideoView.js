6ced1779df4637d6d97aee1401e455ec67a58091
tongchao1 <tongchao1@xiaomi.com>
2020-09-14 19:13:09 +0800

    合入广州宏视全景view代码 refer ${gitlabAfter}

--- miot-sdk/ui/HSVideoView.js
+++ miot-sdk/ui/HSVideoView.js
@@ -1,97 +1,60 @@
 import React from 'react';
-import { requireNativeComponent, UIManager, findNodeHandle } from "react-native";
+import { requireNativeComponent, UIManager, findNodeHandle, ViewPropTypes } from "react-native";
 import PropTypes from 'prop-types';
-import HSVideoViewModule from './HSVideoViewModule';
 const RCTHSVideoView = requireNativeComponent("RCTHSVideoView");
-const kPlayProgressCallback = 'playProgressCallback';
-const kPlayTimestampCallback = 'playTimestampCallback';
-const kVideoViewTouchCallback = 'videoViewTouchCallback';
-const kPlayCompletedCallback = 'playCompletedCallback';
-const kVideoRenderCallback = 'videoRenderCallback';
 export default class HSVideoView extends React.Component {
+  static propTypes = {
+    style: PropTypes.any,
+    camParams: PropTypes.object,
+    fixType: PropTypes.number,
+    displayMode: PropTypes.number,
+    source: PropTypes.string,
+    isPlaying: PropTypes.bool,
+    paused: PropTypes.bool,
+    muted: PropTypes.bool,
+    isScaleZoom: PropTypes.bool,
+    fullscreenState: PropTypes.bool,
+    onVideoViewClick: PropTypes.func,
+    onProgress: PropTypes.func,
+    onTimestamp: PropTypes.func,
+    onRender: PropTypes.func,
+    onCompleted: PropTypes.func,
+    onScreenshot: PropTypes.func,
+    onError: PropTypes.func,
+    ...ViewPropTypes
+  }
   constructor(props) {
     super(props);
     this.videoView = null;
-    this.playProgressCallback = null;
-    this.playTimestapmCallback = null;
-    this.playCompletedCallback = null;
-    this.videoViewTouchCallback = null;
-    this.videoRenderCallBack = null;
-  }
-  static propTypes = {
-    style: PropTypes.any,
-    onVideoViewTouch: PropTypes.func
   }
   static defaultProps = {
-    onVideoViewTouch: null
+    onVideoViewClick: null,
+    onProgress: null,
+    onTimestamp: null,
+    onRender: null,
+    onCompleted: null,
+    onScreenshot: null
   }
   render() {
-    return(
+    return (
       <RCTHSVideoView 
         style={this.props.style}
         ref={(ref) => { this.videoView = ref; }}
+        {...this.props}
       />
-    )
-  }
-  setDisplayMode(type) {
-    HSVideoViewModule.setDisplayMode(type);
-  }
-  setCamParam(camType, panoX, panoY, panoR) {
-    HSVideoViewModule.setCamParam(camType, panoX, panoY, panoR);
-  }
-  setVideoSize(width, height) {
-    HSVideoViewModule.setVideoSize(width, height);
-  }
-  setFixType(type) {
-    HSVideoViewModule.setFixType(type);
-  }
-  renderYUVData(data, width, height) {
-    HSVideoViewModule.renderYUVData(data, width, height);
-  }
-  resetZoomScale() {
-    HSVideoViewModule.resetZoomScale();
-  }
-  cleanSurface() {
-    HSVideoViewModule.cleanSurface();
+    );
   }
   screenshot(type, path) {
-    return new Promise((resolve, reject) => {
-      HSVideoViewModule.screenshot(type, path).then((result) => {
-        resolve(result);
-      });
-    }).catch((error) => {
-      reject(error);
-    });
-  }
-  isPlaying() {
-    return new Promise((resolve, reject) => {
-      HSVideoViewModule.isPlaying().then((result) => {
-        resolve(result);
-      });
-    }).catch((error) => {
-      reject(error);
-    })
-  }
-  startVideo(path) {
-    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.startVideo, [path]);
-  }
-  stopVideo() {
-    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.stopVideo, []);
-  }
-  resumeVideo() {
-    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.resumeVideo, []);
-  }
-  pauseVideo() {
-    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.pauseVideo, []);
-  }
-  startAudio() {
-    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.startAudio, []);
-  }
-  stopAudio() {
-    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.stopAudio, []);
+    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.screenshot, [type, path]);
   }
   seekTo(time) {
     UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.seekTo, [time]);
+  }
+  resetZoomScale() {
+    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.resetZoomScale, []);
+  }
+  cleanSurface() {
+    UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.cleanSurface, []);
   }
   release() {
     UIManager.dispatchViewManagerCommand(findNodeHandle(this.videoView), UIManager.RCTHSVideoView.Commands.release, []);
