73eb4bdae02a108acf501d07613901c15122b114
miot-robot <miot-robot@xiaomi.com>
2020-09-28 18:01:17 +0800

    Merge branch 1.3.2_SDK_10045 into SDK_10045 refer b0d8d2d7312badae30e7468887e2dad78bf25f72

---
+++ miot-sdk/system/network.js
@@ -0,0 +1,45 @@
+/**
+ * @export public
+ * @doc_name 手机网络模块
+ * @doc_index 12
+ * @doc_directory system
+ * @module miot/system
+ * @description
+ * 手机网络相关接口
+ *
+ * @example
+ * import {System} from "miot"
+ * ...
+ * System.network.getGatewayIpAddress().then()
+ * ...
+ */
+import native, { isIOS } from "../native";
+/**
+ * 网络
+ * @interface
+ *
+ */
+class INetwork {
+  /**
+   * 获取手机当前连接的路由器IP地址
+   * @since 10045
+   * @returns {Promise<object>} result:
+   * 成功时：{"code":0, "data":xxx},data.ipAddress:手机当前连接的路由器IP地址
+   * 失败时：{"code":-1, "message":"xxx" }
+   * @example
+   *  System.network.getGatewayIpAddress().then((res) => {
+    if (res && res.data) {
+      alert(`getGatewayIpAddress success,ipAddress:${ res.data.ipAddress }`);
+    } else {
+      alert(`getGatewayIpAddress fail,${ JSON.stringify(res) }`);
+    }
+  }).catch((error) => {
+    alert(`getGatewayIpAddress fail,${ JSON.stringify(error) }`);
+  });
+   */
+  @report
+  getGatewayIpAddress() {
+  }
+}
+const NetworkInstance = new INetwork();
+export default NetworkInstance;
