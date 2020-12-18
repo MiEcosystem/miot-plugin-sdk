68b4a4df82db40424b3ac29c93b815b837322cbd
miot-robot <miot-robot@xiaomi.com>
2020-12-18 19:14:53 +0800

    Merge branch 10049-writenfc into SDK_10049 refer 3fd345774e745fdfc30369aae20538b74a91cce7

--- miot-sdk/host/ui.js
+++ miot-sdk/host/ui.js
@@ -675,6 +675,9 @@ class IUi {
    * @since 10049
    * 打开NFC写设备数据的页面，默认会写入设备的基本信息(如did，model等)，如果插件还需要写入其他数据，可以通过参数extra传给App
    * @param {string} extra 需要写入到nfc设备的额外数据
+   * @example
+   * let extra = 'test_data';
+   * Host.ui.openNFCWriteDeviceInfoPage(extra);
    */
   @report
   openNFCWriteDeviceInfoPage(extra = '') {
