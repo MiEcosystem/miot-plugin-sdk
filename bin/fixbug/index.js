70c6370d9030c1a6992b7ee783d8884485273b1a
miot-robot <miot-robot@xiaomi.com>
2021-01-26 16:14:14 +0800

    Merge branch zxh_fixSafeAreaView into SDK_10050 refer c5f1d2216d3879420cf63b21207f6678610652c1

--- bin/fixbug/index.js
+++ bin/fixbug/index.js
@@ -34,6 +34,7 @@ map.set("node_modules/react-navigation/src/views/TouchableItem.js", "ReactNaviga
 map.set("node_modules/react-navigation-stack/dist/views/TouchableItem.js", "ReactNavigationTouchableItem");
 /** 修复了react-native-svg无法正常显示DynamicColor的BUG */
 map.set("node_modules/react-native-svg/lib/extract/extractBrush.js", "svgExtractBrush");
+map.set("node_modules/react-native-safe-area-view/index.js", "react-safeAreaView");// 修复navigationBar中使用的safeAreaView不支持iphone12系列的bug
 
 module.exports = {
 
