67147387ad9f55e8d4ed0ce15099cf0fe439166a
miot-robot <miot-robot@xiaomi.com>
2021-01-28 10:19:07 +0800

    Update package.json refer 7c02fef2d10c079d2a657ea7433bb9136d91c161

--- miot-sdk/index.js
+++ miot-sdk/index.js
@@ -35,7 +35,7 @@ import ResourcesPack from './resources';
 import ServiceInstance from './Service';
 import { SceneType as SceneTypeNames } from './service/scene';
 import DarkModeFactory from './darkmode';
-export const API_LEVEL = 10050
+export const API_LEVEL = 10051
 /**
  * 插件包基本配置
  * {@link module:miot/Package}
