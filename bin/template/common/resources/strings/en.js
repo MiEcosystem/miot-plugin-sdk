44117ee69e5fa233c62a885d6c99c12340d5cb92
idm-mac <idm@idm-macdeMac-mini.local>
2021-03-29 15:29:26 +0800

    chore: debuging refer ${gitlabAfter}

--- bin/template/common/resources/strings/en.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  hello: 'hello，welcome to MIOT Extension development !',
-  selfDefineScene: 'custom scene',
-  timeSetting: 'time setting',
-  countDownTime: 'countdown time'
-};
