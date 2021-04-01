a20830a62760a2b841ce9ce3ebe7b9abcde2142d
idm-mac <idm@idm-macdeMac-mini.local>
2021-04-01 14:53:41 +0800

    Merge branch origin/SDK_10053_pincode into SDK_10053 refer 4a059766fb2f90b128f4a90908d873a8df3d79d7

--- projects/com.xiaomi.demo/Main/ThirdPartDemo/opencv/CvFaceLandmarksDemo.js
+++ projects/com.xiaomi.demo/Main/ThirdPartDemo/opencv/CvFaceLandmarksDemo.js
@@ -8,49 +8,47 @@
  * @description ==> face detection app for CvCamera
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, View, DeviceEventEmitter, TouchableOpacity, Image} from 'react-native';
-import {CvCamera, CvInvoke} from 'react-native-opencv3';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, View, DeviceEventEmitter, TouchableOpacity, Image } from 'react-native';
+import { CvCamera, CvInvoke } from 'react-native-opencv3';
 
 import { Package } from 'miot';
 
 export default class CvFaceLandmarksDemo extends Component {
   constructor(props) {
-    super(props)
-    this.state = { faces : '',
-      facing : 'back',
-      opencvinstalled:false
-    }
+    super(props);
+    this.state = { faces: '',
+      facing: 'back',
+      opencvinstalled: false
+    };
   }
 
   componentDidMount = () => {
     DeviceEventEmitter.addListener('onFacesDetectedCv', this.onFacesDetectedCv);
-    Package.installModule('RnOpencv').then(res=>{
-      if(res && res.data && res.data.installed){
-        this.setState({opencvinstalled:true});
+    Package.installModule('RnOpencv').then((res) => {
+      if (res && res.data && res.data.installed) {
+        this.setState({ opencvinstalled: true });
       }
-    })
+    });
   }
 
   switchFacing = (e) => {
     if (this.state.facing === 'back') {
-      this.setState({ facing : 'front' })
-    }
-    else {
-      this.setState({ facing : 'back' })
+      this.setState({ facing: 'front' });
+    } else {
+      this.setState({ facing: 'back' });
     }
   }
 
   onFacesDetectedCv = (e) => {
-    //alert('payload: ' + JSON.stringify(e.payload))
+    // alert('payload: ' + JSON.stringify(e.payload))
     if (Platform.OS === 'ios') {
       if ((!e.nativeEvent.payload && this.state.faces) || (e.nativeEvent.payload && !this.state.faces) || (e.nativeEvent.payload && this.state.faces)) {
-        this.setState({ faces : e.nativeEvent.payload })
+        this.setState({ faces: e.nativeEvent.payload });
       }
-    }
-    else {
+    } else {
       if ((!e.payload && this.state.faces) || (e.payload && !this.state.faces) || (e.payload && this.state.faces)) {
-        this.setState({ faces : e.payload })
+        this.setState({ faces: e.payload });
       }
     }
   }
@@ -58,16 +56,16 @@ export default class CvFaceLandmarksDemo extends Component {
   renderLandmarks() {
 
     if (this.state.faces) {
-      const facesJSON = JSON.parse(this.state.faces)
+      const facesJSON = JSON.parse(this.state.faces);
 
-      const faces = facesJSON.faces
-      var allPoints = []
+      const faces = facesJSON.faces;
+      let allPoints = [];
 
-      for (var i = 0; i < faces.length; i++) {
-        const landmarks = faces[i].landmarks
-        for (var j = 0; j < landmarks.length; j++) {
-            const landmarkJson = landmarks[j]
-            allPoints.push(landmarkJson)
+      for (let i = 0; i < faces.length; i++) {
+        const landmarks = faces[i].landmarks;
+        for (let j = 0; j < landmarks.length; j++) {
+          const landmarkJson = landmarks[j];
+          allPoints.push(landmarkJson);
         }
       }
 
@@ -75,33 +73,34 @@ export default class CvFaceLandmarksDemo extends Component {
       let views = allPoints.map((landmark, i) => {
         if (landmark) {
           let box = {
-              position: 'absolute',
-              top: `${100.0*landmark.y}%`,
-              left: `${100.0*landmark.x}%`,
-              width: 3,
-              height: 3,
-              borderWidth: 2,
-              borderColor: '#ff0'
-          }
-          let vertexKey = 'Vertex' + i
+            position: 'absolute',
+            top: `${ 100.0 * landmark.y }%`,
+            left: `${ 100.0 * landmark.x }%`,
+            width: 3,
+            height: 3,
+            borderWidth: 2,
+            borderColor: '#ff0'
+          };
+          let vertexKey = `Vertex${ i }`;
           return (
             <View key={vertexKey} style={box}/>
-          )
+          );
         }
-      })
-      return <View style={styles.allFaceBoxes}>{views}</View>
+        return null;
+      });
+      return <View style={styles.allFaceBoxes}>{views}</View>;
     }
   }
 
   render() {
-    if(this.state.opencvinstalled){
+    if (this.state.opencvinstalled) {
       return (
         <View style={styles.preview}>
           <CvCamera
             style={styles.preview}
             facing={this.state.facing}
-            faceClassifier='haarcascade_frontalface_alt2'
-            landmarksModel='lbfmodel'
+            faceClassifier="haarcascade_frontalface_alt2"
+            landmarksModel="lbfmodel"
             onFacesDetectedCv={this.onFacesDetectedCv}
           />
           {this.renderLandmarks()}
@@ -109,67 +108,67 @@ export default class CvFaceLandmarksDemo extends Component {
             <Image style={Platform.OS === 'android' ? styles.androidImg : styles.iosImg} source={require('./images/flipCamera.png')}/>
           </TouchableOpacity>
         </View>
-      )
-    }else{
+      );
+    } else {
       return (
         <View style={styles.preview}/>
-      )
+      );
     }
   }
 }
 
 const styles = StyleSheet.create({
   androidImg: {
-    transform : [{ rotate: '-90deg' }],
-    backgroundColor : 'transparent',
-    width : 50,
-    height : 50
+    transform: [{ rotate: '-90deg' }],
+    backgroundColor: 'transparent',
+    width: 50,
+    height: 50
   },
   iosImg: {
-    backgroundColor : 'transparent',
-    width : 50,
-    height : 50
+    backgroundColor: 'transparent',
+    width: 50,
+    height: 50
   },
   androidButton: {
-    top : 0,
-    bottom : 0,
-    right : 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
     width: '10%',
-    position : 'absolute',
-    backgroundColor : '#FFF',
-    opacity : 0.75,
-    display : 'flex',
-    justifyContent : 'center',
-    alignItems : 'center'
+    position: 'absolute',
+    backgroundColor: '#FFF',
+    opacity: 0.75,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   iosButton: {
-    left : 0,
-    right : 0,
-    bottom : 0,
-    height : '10%',
-    position : 'absolute',
-    backgroundColor : '#FFF',
-    opacity : 0.75,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: '10%',
+    position: 'absolute',
+    backgroundColor: '#FFF',
+    opacity: 0.75,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center'
   },
   allFaceBoxes: {
-    backgroundColor : 'transparent',
-    position : 'absolute',
-    alignItems : 'center',
-    top : 0,
-    left : 0,
-    width : '100%',
-    height : '100%'
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    alignItems: 'center',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%'
   },
   preview: {
-    alignItems : 'center',
-    backgroundColor : 'transparent',
-    top : 0,
-    left : 0,
-    right : 0,
-    bottom : 0,
-    position : 'absolute'
-  },
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    position: 'absolute'
+  }
 });
