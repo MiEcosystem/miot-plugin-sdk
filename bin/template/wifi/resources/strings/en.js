ef95153599407586ef3d48f949a38429bd911c52
Xiaomi-Yule <yule1@xiaomi.com>
2021-04-19 09:48:40 +0800

    sync code 10053

---
+++ bin/template/wifi/resources/strings/en.js
@@ -0,0 +1,6 @@
+module.exports = {
+  hello: 'hello，welcome to MIOT Extension development !',
+  selfDefineScene: 'custom scene',
+  timeSetting: 'time setting',
+  countDownTime: 'countdown time'
+};
