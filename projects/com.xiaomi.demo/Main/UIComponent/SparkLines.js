da1edd85aa87635adcf938dfce2da0253c47cddc
miot-robot <miot-robot@xiaomi.com>
2021-11-05 19:41:48 +0800

    Merge branch shoupengpeng3-SDK_10061-patch-90924 into SDK_10061 refer 8d641f4183a43e4a89421826c6b5f92c2794238d

--- projects/com.xiaomi.demo/Main/UIComponent/SparkLines.js
+++ projects/com.xiaomi.demo/Main/UIComponent/SparkLines.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SparkLines from "miot/ui/Sparkline";
 
-import { ScrollView } from "react-native";
+import { ScrollView, View, Text, StyleSheet } from "react-native";
 
 export default class StringSpinnerDemo extends React.Component {
   constructor(props) {
@@ -20,10 +20,10 @@ export default class StringSpinnerDemo extends React.Component {
                 // 每一个渐变颜色对应的渐变区域的最大最小值，只有单一值的话另一值则为无穷
                 { min: 150 },
                 { max: 150, min: 75 },
-                { min: 0, max: 75 },
-              ],
+                { min: 0, max: 75 }
+              ]
               // direction字段可配置渐变方向
-            },
+            }
           ],
           labels: [
             "1月",
@@ -34,7 +34,7 @@ export default class StringSpinnerDemo extends React.Component {
             "6月",
             "7月",
             "8月",
-            "9月",
+            "9月"
           ], // x轴坐标文字
           unit: "μg/m³", // 选中数据单位
           title: "空气净化器 要求样式", // 曲线标题
@@ -45,27 +45,27 @@ export default class StringSpinnerDemo extends React.Component {
           visual: [
             { name: "优", color: "#00B7A2" },
             { name: "良", color: "#FFA943" },
-            { name: "差", color: "#FC675F" },
+            { name: "差", color: "#FC675F" }
           ],
           fixed: 2, // 选中数据保留小数位数
-          scale: false, // y轴是否按照数值缩放
+          scale: false // y轴是否按照数值缩放
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v + v),
-              name: "a",
+              name: "b",
               gradient: ["#00B7A2", "#FFA943", "#FC675F"],
               range: "line", // 渐变颜色应用在曲线所在范围内，global则为整个y轴范围
-              format: (v) => `${v.replace(v[1], Number(v[1]) - 1)}-${v} 最高`, // 选中数据提示文字格式化函数
+              format: (v) => `${ v.replace(v[1], Number(v[1]) - 1) }-${ v } 最高` // 选中数据提示文字格式化函数
             },
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "b",
+              name: "c",
               gradient: ["#00B7A2", "#FFA943", "#FC675F"],
               range: "line",
-              format: (v) => `${v.replace(v[1], Number(v[1]) - 1)}-${v} 最低`,
-            },
+              format: (v) => `${ v.replace(v[1], Number(v[1]) - 1) }-${ v } 最低`
+            }
           ],
           labels: [
             "01:00",
@@ -76,7 +76,7 @@ export default class StringSpinnerDemo extends React.Component {
             "06:00",
             "07:00",
             "08:00",
-            "09:00",
+            "09:00"
           ],
           unit: "μg/m³",
           title: "无叶净化器要求样式",
@@ -87,20 +87,20 @@ export default class StringSpinnerDemo extends React.Component {
           visual: [
             { name: "优", color: "#00B7A2" },
             { name: "良", color: "#FFA943" },
-            { name: "差", color: "#FC675F" },
+            { name: "差", color: "#FC675F" }
           ],
           fixed: 2,
-          scale: false,
+          scale: false
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "a",
+              name: "d",
               gradient: ["rgba(69, 180, 255, 0)", "#A9DCFF"],
               color: "#45B4FF", // 填充状态下曲线颜色
-              format: "{l} 温度", // 选中数据提示文字格式化，{l}会被替换
-            },
+              format: "{l} 温度" // 选中数据提示文字格式化，{l}会被替换
+            }
           ],
           labels: [
             "1月",
@@ -111,7 +111,7 @@ export default class StringSpinnerDemo extends React.Component {
             "6月",
             "7月",
             "8月",
-            "9月",
+            "9月"
           ],
           unit: "℃",
           title: "温湿度要求样式1",
@@ -121,22 +121,22 @@ export default class StringSpinnerDemo extends React.Component {
           selectType: "line",
           fill: true, // 填充，填充状态下，渐变色自动转化为填充渐变
           fixed: 1,
-          scale: false,
+          scale: false
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v + v),
-              name: "a",
+              name: "e",
               gradient: ["#17A0FD"], // 由于样式配置内容繁杂，曲线图单色也用gradient配置
-              format: (v) => `${v.replace(v[1], Number(v[1]) - 1)}-${v} 最高`,
+              format: (v) => `${ v.replace(v[1], Number(v[1]) - 1) }-${ v } 最高`
             },
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "b",
+              name: "f",
               gradient: ["#85CCFD"],
-              format: (v) => `${v.replace(v[1], Number(v[1]) - 1)}-${v} 最低`,
-            },
+              format: (v) => `${ v.replace(v[1], Number(v[1]) - 1) }-${ v } 最低`
+            }
           ],
           labels: [
             "01:00",
@@ -147,7 +147,7 @@ export default class StringSpinnerDemo extends React.Component {
             "06:00",
             "07:00",
             "08:00",
-            "09:00",
+            "09:00"
           ],
           unit: "℃",
           title: "温湿度要求样式2",
@@ -157,18 +157,18 @@ export default class StringSpinnerDemo extends React.Component {
           fixed: 0,
           visual: [
             { name: "最高", color: "#17A0FD" },
-            { name: "最低", color: "#85CCFD" },
-          ],
+            { name: "最低", color: "#85CCFD" }
+          ]
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "a",
+              name: "g",
               gradient: ["rgba(69, 180, 255, 0)", "#A9DCFF"],
               color: "#45B4FF",
-              format: "纯水水质",
-            },
+              format: "纯水水质"
+            }
           ],
           labels: [
             "01:00",
@@ -179,7 +179,7 @@ export default class StringSpinnerDemo extends React.Component {
             "06:00",
             "07:00",
             "08:00",
-            "09:00",
+            "09:00"
           ],
           unit: "TDS",
           title: "TDS水质样式要求",
@@ -187,17 +187,17 @@ export default class StringSpinnerDemo extends React.Component {
           status: "normal",
           selectType: "line",
           fixed: 0,
-          fill: true,
+          fill: true
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "a",
+              name: "h",
               gradient: ["rgba(150, 120, 250, 0)", "#9678FA"],
               color: "#9678FA",
-              format: "{l}功率",
-            },
+              format: "{l}功率"
+            }
           ],
           labels: [
             "1月",
@@ -208,7 +208,7 @@ export default class StringSpinnerDemo extends React.Component {
             "6月",
             "7月",
             "8月",
-            "9月",
+            "9月"
           ],
           unit: "w",
           title: "功率样式要求",
@@ -218,17 +218,17 @@ export default class StringSpinnerDemo extends React.Component {
           fixed: 0,
           scale: false,
           fill: true,
-          smooth: true,
+          smooth: true
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v + v),
-              name: "a",
+              name: "i",
               color: "#36CF6E",
               gradient: ["rgba(54, 207, 110, 0)", "#36CF6E"],
-              format: (v) => `${v.replace(v[1], Number(v[1]) - 1)}-${v} 气压值`,
-            },
+              format: (v) => `${ v.replace(v[1], Number(v[1]) - 1) }-${ v } 气压值`
+            }
           ],
           labels: [
             "01:00",
@@ -239,7 +239,7 @@ export default class StringSpinnerDemo extends React.Component {
             "06:00",
             "07:00",
             "08:00",
-            "09:00",
+            "09:00"
           ],
           unit: "KPa",
           title: "气压值要求样式",
@@ -249,16 +249,16 @@ export default class StringSpinnerDemo extends React.Component {
           fixed: 1,
           scale: false,
           smooth: true,
-          fill: true,
+          fill: true
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "a",
+              name: "j",
               color: "#545AFA",
-              format: "{l}用电电量",
-            },
+              format: "{l}用电电量"
+            }
           ],
           labels: [
             "1月",
@@ -269,22 +269,22 @@ export default class StringSpinnerDemo extends React.Component {
             "6月",
             "7月",
             "8月",
-            "9月",
+            "9月"
           ],
           unit: "KW/h",
           title: "电量统计要求样式",
           type: "bar",
           status: "normal",
-          fixed: 1,
+          fixed: 1
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "a",
+              name: "k",
               color: "#FFA626",
-              format: "{l}用电电量",
-            },
+              format: "{l}用电电量"
+            }
           ],
           labels: [
             "1月",
@@ -295,28 +295,28 @@ export default class StringSpinnerDemo extends React.Component {
             "6月",
             "7月",
             "8月",
-            "9月",
+            "9月"
           ],
           unit: "KW/h",
           title: "耗电量要求样式",
           type: "bar",
           status: "normal",
-          fixed: 1,
+          fixed: 1
         },
         {
           value: [
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v + v),
-              name: "a",
+              name: "l",
               color: "#23DEFF",
-              format: "{l}用电量",
+              format: "{l}用电量"
             },
             {
               data: new Array(9).fill(175).map((v) => Math.random() * v),
-              name: "a",
+              name: "m",
               color: "#08C5FC",
-              format: "{l}用电量",
-            },
+              format: "{l}用电量"
+            }
           ],
           labels: [
             "1月",
@@ -327,15 +327,53 @@ export default class StringSpinnerDemo extends React.Component {
             "6月",
             "7月",
             "8月",
-            "9月",
+            "9月"
           ],
-          unit: "KW/h",
-          title: "电量统计要求样式",
+          unit: "L",
+          title: "用水详情要求样式",
+          type: "bar",
+          status: "normal",
+          fixed: 1
+        },
+        {
+          value: [
+            {
+              data: new Array(9).fill(175).map((v) => Math.random() * v + v),
+              name: "n",
+              color: "#23DEFF",
+              format: "{l}用电量"
+            }
+          ],
+          labels: [
+            "1月",
+            "2月",
+            "3月",
+            "4月",
+            "5月",
+            "6月",
+            "7月",
+            "8月",
+            "9月"
+          ],
+          unit: "L",
+          title: "选中文字插槽",
           type: "bar",
           status: "normal",
           fixed: 1,
-        },
-      ],
+          itemWidth: 20, // 配置柱状图宽度，默认14，
+          viewslot: (
+            <View>
+              <View style={styles.row}>
+                <Text style={styles.number}>2</Text>
+                <Text style={styles.unit}>小时</Text>
+                <Text style={styles.number}>0</Text>
+                <Text style={styles.unit}>分钟</Text>
+              </View>
+              <Text style={styles.label}>使用时长</Text>
+            </View>
+          )
+        }
+      ]
     };
   }
 
@@ -350,3 +388,29 @@ export default class StringSpinnerDemo extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  number: {
+    fontFamily: "Xiaomi Prototype 210317",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: 30,
+    height: 36,
+    color: "#000000"
+  },
+  label: {
+    fontStyle: "normal",
+    fontSize: 12,
+    lineHeight: 16,
+    color: "rgba(0, 0, 0, 0.4)",
+    textAlign: "right"
+  },
+  unit: {
+    marginLeft: 2,
+    fontStyle: "normal",
+    fontSize: 12,
+    height: 16,
+    color: "#000000"
+  },
+  row: { flexDirection: "row", alignItems: "baseline" }
+});
