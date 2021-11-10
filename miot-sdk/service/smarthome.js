ab9b65959fea58e2d7831faccd83bf9a4a50f9c2
miot-robot <miot-robot@xiaomi.com>
2021-11-10 19:48:07 +0800

    Merge branch SDK_10062_getdevicedatav2 into SDK_10062 refer e3d2d50d79dcd8c86622cbc874e4d8bf16fd25ff

--- miot-sdk/service/smarthome.js
+++ miot-sdk/service/smarthome.js
@@ -416,6 +416,35 @@ class ISmartHome {
        return Promise.resolve(null);
     }
     /**
+     * 查询用户名下设备上报的属性和事件
+     * 获取设备属性和事件历史记录，订阅消息直接写入到服务器，不需要插件添加，最多查询90天前的记录。
+     * 对于蓝牙设备，params.key 可参考文档 [米家BLE Object定义](https://iot.mi.com/new/doc/embedded-development/ble/object-definition.html)
+     *
+     * error code:
+     *
+     * | code | desc |
+     * | :-: | --- |
+     * |  0  | 成功 |
+     * | -8  | 请求参数缺失或者类型不对 |
+     * | -4  | 服务器错误 |
+     * | -1  | 请求uid无权限获取did的相关数据 |
+     * @since 10062
+     * @param {json} params -参数\{did,type,key,time_start,time_end,limit}含义如下：
+     * @param {string} params.did 设备id。 必选参数
+     * @param {string} params.key 属性或事件名，可选参数。(注意：如果设备是蓝牙设备，传入的是object id， 且为十进制数据；如果是wifi设备，才传入自定义属性或事件名，可以在开发者平台-产品-功能定义中查看)，如果是miot-spec设备，请传入（siid.piid或者siid.eiid）
+     * @param {string} params.type 必选参数[prop/event], 如果是查询上报的属性则type为prop，查询上报的事件则type为event,
+     * @param {number} params.time_start 数据起点，单位是秒。必选参数
+     * @param {number} params.time_end 数据终点，单位是秒。必选参数，time_end必须大于time_start,
+     * @param {string} params.group 返回数据的方式，默认raw,可选值为hour、day、week、month。可选参数.
+     * @param {string} params.limit 返回数据的条数，默认20，最大1000。可选参数.
+     * @param {number} params.uid 要查询的用户id 。可选参数
+     * @returns {Promise}
+     */
+    @report
+    getDeviceDataV2(params) {
+       return Promise.resolve(null);
+    }
+    /**
      * 删除用户的设备信息（prop和event 除外）.
      * 删除对应时间戳的上报的数据，无法删除type为prop和event,删除后可用get_user_device_data校验。
      * 如果get_user_device_data校验返回的为[]表示删除成功。
