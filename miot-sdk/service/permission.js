fcf076be286e09a977a2ca690bc2d7863a14aeca
miot-robot <miot-robot@xiaomi.com>
2021-11-24 15:21:10 +0800

    Merge branch feat/add into SDK_10063 refer ${gitlabAfter}

--- miot-sdk/service/permission.js
+++ miot-sdk/service/permission.js
@@ -12,6 +12,7 @@
  */
 import { report } from "../decorator/ReportDecorator";
 import native from "../native";
+import permissionLocal from "./permissionLocal.json";
 class IPermission {
   /**
    * 拉取最新远程配置，默认会调用一次，优先使用远程配置
