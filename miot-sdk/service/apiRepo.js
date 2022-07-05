c02ba352330394e2c4dffc482b14f513127414ca
miot-robot <miot-robot@xiaomi.com>
2022-07-05 11:34:43 +0800

    Merge branch cherry-pick-5c26cd89 into SDK_10072 refer 3b3519a80cc173acec3baea1cc22cd4bfc3862d5

--- miot-sdk/service/apiRepo.js
+++ miot-sdk/service/apiRepo.js
@@ -290,6 +290,8 @@ const API_REPO = {
   // 获取中枢的列表
   '/v2/device/get_lan_hub': {},
   // 获取自动更新列表
-  '/v2/device/auto_upgrade_list': {}
+  '/v2/device/auto_upgrade_list': {},
+  // 获取预置音频
+  '/app/data/v2/preDataGetCommon': {}
 };
 export default API_REPO;
