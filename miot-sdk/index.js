f550b8b0e5ad2779bf22b61abeade8d8e1b5a47d
miot-robot <miot-robot@xiaomi.com>
2022-07-25 16:13:58 +0800

    [Feature]: 添加定时2.0界面接口 refer fad21f84c35e24f8e2d59dd468db7d45d6c45173

--- miot-sdk/index.js
+++ miot-sdk/index.js
@@ -36,7 +36,7 @@ import ResourcesPack from './resources';
 import ServiceInstance from './Service';
 import { SceneType as SceneTypeNames } from './service/scene';
 import DarkModeFactory from './darkmode';
-export const API_LEVEL = 10073
+export const API_LEVEL = 10074
 /**
  * 插件包基本配置
  * {@link module:miot/Package}
