4d108000134859cb1407afdb96c8ca1905aa7533
miot-robot <miot-robot@xiaomi.com>
2022-08-17 15:39:17 +0800

    Merge branch 10074_feat_auth into SDK_10074 refer 81c6e5c11404db55c53df33dbcea2a55a0bd9af7

--- miot-sdk/service/xiaoai.js
+++ miot-sdk/service/xiaoai.js
@@ -75,6 +75,33 @@
   fetchQQMusicAccountInfo(params = {}) {
      return Promise.resolve(null);
   }
+  /**
+   * @since 10074
+   * 小爱服务端请求服务(非音箱)(http://passport.d.xiaomi.net/doc/oauth/oauth-v2-api.html 授权码模式鉴权 会弹出授权确认框)
+   * @param {jsonObject} params 传递的jsonObject对象参数
+   * @example
+   * let params={
+   *  clientId: xx,           ||设备在小爱开放平台注册的id 必填
+   *  redirectUrl: xx,        ||设备在小爱开放平台注册的回调地址 必填
+   *  clientSecret: xx,       ||设置在小爱开放平台生成的app_secret 必填
+   *  requestUrl: xx,         ||请求的接口路径(完整的接口路径包括host和path 例如https://xx/xx) 必填
+   *  requestType: xx,        ||请求方式 get/post/put/delete 不传默认为get方式
+   *  requestParams: xx       ||请求接口对应的参数 (jsonObject)
+   *  contentType: xx         ||contentType put和post方法默认是以表单方式提交参数，即Content-Type为application/x-www-form-urlencoded，如果想以application/json的方式，请传入'json'
+   * }
+   * service.xiaoai.callXiaoaiServiceAPI(params);
+   *  * @returns {object} 成功时，返回：
+   * { code: 0,
+   *    data: {
+   *      //接口返回的透传数据
+   *    }
+   * }
+   * 失败时，返回：透传
+   * { code: xx, message: 'xx' }
+   */
+  callXiaoaiServiceAPI(params = {}) {
+   return Promise.resolve(null);
+  }
 }
 const IXiaoaiInstance = new IXiaoai();
 export default IXiaoaiInstance;
