01f586f30f318a6f4927bcccdd7be055e4f5cbb0
miot-robot <miot-robot@xiaomi.com>
2022-08-24 11:26:06 +0800

    Merge branch feat_switch_setting into SDK_10074 refer 91dfaee5ce413866f587864261794bca91ba535d

--- miot-sdk/ui/Settings/FeatureSettings.js
+++ miot-sdk/ui/Settings/FeatureSettings.js
@@ -9,25 +9,39 @@ import getItems, { getAllAndDefaultOptions, itemPropTypes } from './getItems';
 import useGatewayStatus, { State as GatewayStatus } from '../../hooks/useGatewayStatus';
 import useMultiKeySplitInfo from '../../hooks/useMultiKeySplitInfo';
 import { specPluginNames } from '../../utils/special-plugins';
+import useMemberSetInfo from '../../hooks/useMemberSetInfo';
+import ListItem from '../ListItem/ListItem';
 const innerOptions = {
   // 按键设置
   memberSet: {
     exportKey: 'MEMBER_SET',
-    isDefault: false,
+    isDefault: true,
     ownerOnly: true,
-    modelTypes: ['switch'],
-    title: I18n.memberSet,
-    onPress: () => {
-      const { packageName } = Package;
-      const { deviceID, mac } = Device;
-      if (specPluginNames.includes(packageName)) {
-        Host.ui.openPowerMultikeyPage(deviceID, mac, {
-          useNewSetting: true,
-          done: []
-        });
-      } else {
-        Host.ui.openPowerMultikeyPage(deviceID, mac);
+    modelTypes: ['switch', 'control-panel', 'relay', 'controller'],
+    Component: () => {
+      const showMemberSet = useMemberSetInfo().showMemberSetKey; // bool值，决定是否显示 按键设置
+      const onPress = () => {
+        const { packageName } = Package;
+        const { deviceID, mac } = Device;
+        Host.ui.openPowerMultikeyPage(deviceID, mac, specPluginNames.includes(packageName) ?
+          {
+            useNewSetting: true,
+            done: []
+          } : undefined);
+      };
+      if (!showMemberSet) {
+        return null;
       }
+      return (
+        <ListItem
+          key={'memberSet'}
+          title={I18n.memberSet}
+          value=""
+          onPress={onPress}
+          useNewType={true}
+          hideArrow={false}
+        />
+      );
     }
   },
   // 多键拆分
