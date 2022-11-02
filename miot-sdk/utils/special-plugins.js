811e15ebebf3ad310808d1d29994250d1efd269d
miot-robot <miot-robot@xiaomi.com>
2022-11-02 11:40:58 +0800

    添加新的门铃新标准插件适配 refer ${gitlabAfter}

--- miot-sdk/utils/special-plugins.js
+++ miot-sdk/utils/special-plugins.js
@@ -1,1 +1,5 @@
-export const specPluginNames = ['miot.plugin.spec'];
+export const pluginSpecModel = 'miot.switch.spec';
+export const pluginSpecPackagesName = 'miot.plugin.spec';
+export const cameraSpecPackagesName = 'com.xiaomi.stdcamera';
+export const doorbellSpecPackagesName = 'miot.doorbell.spec';
+export const specPluginNames = [pluginSpecPackagesName, cameraSpecPackagesName, doorbellSpecPackagesName];
