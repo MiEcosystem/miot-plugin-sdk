ad9f415209b17e849e0d5c162838b72d011074bf
miot-robot <miot-robot@xiaomi.com>
2023-03-14 14:15:52 +0800

    [bugfix]修复CommonSetting米家首页显示刷新问题IOT-38146 refer ${gitlabAfter}

--- miot-sdk/service/scene.js
+++ miot-sdk/service/scene.js
@@ -472,10 +472,10 @@ class IMiotScene {
      * 打开时间设置页面(米家APP实现)
      * @since 10032 ,SDKLevel 10032 开始提供使用
      * @param {object} options 配置信息
-     * @param {string} options.onMethod 配置定时开启的 method 名，同上面openTimerSettingPageWithVariousTypeParams的参数onMethod
-     * @param {string} options.onParam 配置定时开启的 参数，同上面openTimerSettingPageWithVariousTypeParams的参数onParam
-     * @param {string} options.offMethod 配置定时关闭的 method 名，同上面openTimerSettingPageWithVariousTypeParams的参数offMethod
-     * @param {string} options.offParam 配置定时关闭的 参数，同上面openTimerSettingPageWithVariousTypeParams的参数offParam
+     * @param {string} options.onMethod 配置定时开启的 method 名，spec设备为set_properties
+     * @param {string} options.onParam 配置定时开启的 参数，和Service.spec.setPropertiesValue方法传的参数一致
+     * @param {string} options.offMethod 配置定时关闭的 method 名，spec设备为set_properties
+     * @param {string} options.offParam 配置定时关闭的 参数，和Service.spec.setPropertiesValue方法传的参数一致
      * @param {string} options.timerTitle 定时列表页自定义标题
      * @param {string} options.displayName 配置场景日志显示的名称
      * @param {string} options.identify 自定义定时Identifier
@@ -488,8 +488,9 @@ class IMiotScene {
      * @param {boolean} options.showPeriodTimerType 是否可以创建：时间段定时？ true: 可以，false:不可以(默认：true)
      * @param {string} options.did 设备did，可为空，@since 10045
      * 注意：showOnTimerType、showOffTimerType、showPeriodTimerType三个参数至少有一个为true，才有效，否则三个中任意都会被忽略掉
+     * 10074新增了openTimerSettingPageWithOptionsV2方法，建议使用该方法，不过V2目前不支持本地定时的设备，如果有本地定时需求还是得使用此方法，V2会在后续迭代中支持本地定时
      * @example
-     * Service.scene.openTimerSettingPageWithOptions({onMethod:"power_on", onParam: "on", offMethod: "power_off", offParam: "off", displayName:"设置xxx定时"，identify:"plug_usb_countdowm"})
+     * Service.scene.openTimerSettingPageWithOptions({onMethod:"set_properties", onParam: [{did:Device.deviceID, siid:3, piid:2, value:true}], offMethod: "set_properties", offParam: [{did:Device.deviceID, siid:3, piid:2, value:false}], displayName:"设置xxx定时"，identify:"plug_usb_countdowm"})
      */
     @report
     openTimerSettingPageWithOptions(options) {
@@ -510,8 +511,8 @@ class IMiotScene {
      *
      */
     @report
-    openCountDownPage(isCountDownOn, setting) {
-    }
+     openCountDownPage(isCountDownOn, setting) {
+     }
        return Promise.resolve('');
     }
     /**
--- projects/com.xiaomi.demo/Main/ThirdPartDemo/ReactNativeCameraDemo.js
+++ projects/com.xiaomi.demo/Main/ThirdPartDemo/ReactNativeCameraDemo.js
@@ -4,83 +4,83 @@ import { PermissionsAndroid, StyleSheet, Text, TouchableOpacity, View } from 're
 import Camera, { RNCamera } from 'react-native-camera';
 
 export default class ReactNativeCameraDemo extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <RNCamera
-                    ref={ref => {
-                        this.camera = ref;
-                    }}
-                    style={styles.preview}
-                    type={RNCamera.Constants.Type.back}
-                    flashMode={RNCamera.Constants.FlashMode.on}
-                    androidCameraPermissionOptions={{
-                        title: 'Permission to use camera',
-                        message: 'We need your permission to use your camera',
-                        buttonPositive: 'Ok',
-                        buttonNegative: 'Cancel',
-                    }}
-                    androidRecordAudioPermissionOptions={{
-                        title: 'Permission to use audio recording',
-                        message: 'We need your permission to use your audio',
-                        buttonPositive: 'Ok',
-                        buttonNegative: 'Cancel',
-                    }}
-                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
-                        console.log(barcodes);
-                    }}
-                />
-                <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                    <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-                        <Text style={{ fontSize: 14 }}> SNAP </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel'
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel'
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
-    takePicture = async function () {
-        if (Host.isIOS) {
-            if (!await Camera.checkDeviceAuthorizationStatus()) {
-                alert('相机权限未开启')
-                return
-            }
+    takePicture = async function() {
+      if (Host.isIOS) {
+        if (!await Camera.checkDeviceAuthorizationStatus()) {
+          alert('相机权限未开启');
+          return;
         }
+      }
 
-        if (Host.isAndroid) {
-            if (!await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA)) {
-                alert('相机权限未开启')
-                return
-            } else {
-                console.log('相机权限已开启');
-            }
+      if (Host.isAndroid) {
+        if (!await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA)) {
+          alert('相机权限未开启');
+          return;
+        } else {
+          console.log('相机权限已开启');
         }
-        if (this.camera) {
-            const options = { quality: 0.5, base64: true };
-            const data = await this.camera.takePictureAsync(options);
-            console.log(data.uri);
-        }
+      }
+      if (this.camera) {
+        const options = { quality: 0.5, base64: true };
+        const data = await this.camera.takePictureAsync(options);
+        console.log(data.uri);
+      }
     };
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'black',
-    },
-    preview: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-    },
-    capture: {
-        flex: 0,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        padding: 15,
-        paddingHorizontal: 20,
-        alignSelf: 'center',
-        margin: 20,
-    },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
 });
