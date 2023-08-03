d72ef4b18faa5ed9498861340bf4a55294034ea1
miot-robot <miot-robot@xiaomi.com>
2023-08-03 11:05:36 +0800

    feat: 动态导航 refer ${gitlabAfter}

--- projects/com.xiaomi.demo/CommonModules/MHNavigationBar.js
+++ projects/com.xiaomi.demo/CommonModules/MHNavigationBar.js
@@ -25,22 +25,22 @@
  */
 'use strict';
 
-var React = require('React');
+let React = require('React');
 // var NavigatorNavigationBarStylesAndroid = require('NavigatorNavigationBarStylesAndroid');
-var NavigatorNavigationBarStylesIOS = require('NavigatorNavigationBarStylesIOS');
-var Platform = require('Platform');
-var StyleSheet = require('StyleSheet');
-var View = require('View');
+let NavigatorNavigationBarStylesIOS = require('NavigatorNavigationBarStylesIOS');
+let Platform = require('Platform');
+let StyleSheet = require('StyleSheet');
+let View = require('View');
 
-var guid = require('guid');
+let guid = require('guid');
 
-var { Map } = require('immutable');
+let { Map } = require('immutable');
 
-var COMPONENT_NAMES = ['Title', 'LeftButton', 'RightButton'];
+let COMPONENT_NAMES = ['Title', 'LeftButton', 'RightButton'];
 
-var NavigatorNavigationBarStyles = NavigatorNavigationBarStylesIOS;
+let NavigatorNavigationBarStyles = NavigatorNavigationBarStylesIOS;
 
-var navStatePresentedIndex = function(navState) {
+let navStatePresentedIndex = function(navState) {
   if (navState.presentedIndex !== undefined) {
     return navState.presentedIndex;
   }
@@ -48,31 +48,31 @@ var navStatePresentedIndex = function(navState) {
   return navState.observedTopOfStack;
 };
 
-var MHNavigationBar = React.createClass({
+let MHNavigationBar = React.createClass({
 
   propTypes: {
     navigator: React.PropTypes.object,
     routeMapper: React.PropTypes.shape({
       Title: React.PropTypes.func.isRequired,
       LeftButton: React.PropTypes.func.isRequired,
-      RightButton: React.PropTypes.func.isRequired,
+      RightButton: React.PropTypes.func.isRequired
     }).isRequired,
     navState: React.PropTypes.shape({
       routeStack: React.PropTypes.arrayOf(React.PropTypes.object),
-      presentedIndex: React.PropTypes.number,
+      presentedIndex: React.PropTypes.number
     }),
     navigationStyles: React.PropTypes.object,
-    style: View.propTypes.style,
+    style: View.propTypes.style
   },
 
   statics: {
     Styles: NavigatorNavigationBarStyles,
-    StylesIOS: NavigatorNavigationBarStylesIOS,
+    StylesIOS: NavigatorNavigationBarStylesIOS
   },
 
   getDefaultProps() {
     return {
-      navigationStyles: NavigatorNavigationBarStyles,
+      navigationStyles: NavigatorNavigationBarStyles
     };
   },
 
@@ -95,37 +95,37 @@ var MHNavigationBar = React.createClass({
     this._components = {};
     this._descriptors = {};
 
-    COMPONENT_NAMES.forEach(componentName => {
+    COMPONENT_NAMES.forEach((componentName) => {
       this._components[componentName] = new Map();
       this._descriptors[componentName] = new Map();
     });
   },
 
   _getReusableProps: function(
-    /*string*/componentName,
-    /*number*/index
-  ) /*object*/ {
-    var propStack = this._reusableProps[componentName];
+    /* string */componentName,
+    /* number */index
+  ) /* object */ {
+    let propStack = this._reusableProps[componentName];
     if (!propStack) {
       propStack = this._reusableProps[componentName] = [];
     }
-    var props = propStack[index];
+    let props = propStack[index];
     if (!props) {
-      props = propStack[index] = {style:{}};
+      props = propStack[index] = { style: {} };
     }
     return props;
   },
 
   _updateIndexProgress: function(
-    /*number*/progress,
-    /*number*/index,
-    /*number*/fromIndex,
-    /*number*/toIndex
+    /* number */progress,
+    /* number */index,
+    /* number */fromIndex,
+    /* number */toIndex
   ) {
-    var amount = toIndex > fromIndex ? progress : (1 - progress);
-    var oldDistToCenter = index - fromIndex;
-    var newDistToCenter = index - toIndex;
-    var interpolate;
+    let amount = toIndex > fromIndex ? progress : (1 - progress);
+    let oldDistToCenter = index - fromIndex;
+    let newDistToCenter = index - toIndex;
+    let interpolate;
     if (oldDistToCenter > 0 && newDistToCenter === 0 ||
         newDistToCenter > 0 && oldDistToCenter === 0) {
       interpolate = this.props.navigationStyles.Interpolators.RightToCenter;
@@ -138,9 +138,9 @@ var MHNavigationBar = React.createClass({
       interpolate = this.props.navigationStyles.Interpolators.RightToLeft;
     }
 
-    COMPONENT_NAMES.forEach(function (componentName) {
-      var component = this._components[componentName].get(this.props.navState.routeStack[index]);
-      var props = this._getReusableProps(componentName, index);
+    COMPONENT_NAMES.forEach(function(componentName) {
+      let component = this._components[componentName].get(this.props.navState.routeStack[index]);
+      let props = this._getReusableProps(componentName, index);
       if (component && interpolate[componentName](props.style, amount)) {
         component.setNativeProps(props);
       }
@@ -148,30 +148,28 @@ var MHNavigationBar = React.createClass({
   },
 
   updateProgress: function(
-    /*number*/progress,
-    /*number*/fromIndex,
-    /*number*/toIndex
+    /* number */progress,
+    /* number */fromIndex,
+    /* number */toIndex
   ) {
-    var max = Math.max(fromIndex, toIndex);
-    var min = Math.min(fromIndex, toIndex);
-    for (var index = min; index <= max; index++) {
+    let max = Math.max(fromIndex, toIndex);
+    let min = Math.min(fromIndex, toIndex);
+    for (let index = min; index <= max; index++) {
       this._updateIndexProgress(progress, index, fromIndex, toIndex);
     }
   },
 
   render: function() {
-    var navBarStyle = {
-      height: this.props.navigationStyles.General.TotalNavHeight,
+    let navBarStyle = {
+      height: this.props.navigationStyles.General.TotalNavHeight
     };
-    var navState = this.props.navState;
-    var components = navState.routeStack.map((route, index) => {
-      if (index == navState.routeStack.length-1)
-      {
-        return COMPONENT_NAMES.map(componentName =>
+    let navState = this.props.navState;
+    let components = navState.routeStack.map((route, index) => {
+      if (index == navState.routeStack.length - 1) {
+        return COMPONENT_NAMES.map((componentName) =>
           this._getComponent(componentName, route, index)
-        )
-      }
-      else {
+        );
+      } else {
         return [];
       }
     });
@@ -187,17 +185,17 @@ var MHNavigationBar = React.createClass({
   },
 
   _getComponent: function(
-    /*string*/componentName,
-    /*object*/route,
-    /*number*/index
-  ) /*?Object*/ {
+    /* string */componentName,
+    /* object */route,
+    /* number */index
+  ) /* ?Object */ {
     if (this._descriptors[componentName].includes(route)) {
       return this._descriptors[componentName].get(route);
     }
 
-    var rendered = null;
+    let rendered = null;
 
-    var content = this.props.routeMapper[componentName](
+    let content = this.props.routeMapper[componentName](
       this.props.navState.routeStack[index],
       this.props.navigator,
       index,
@@ -207,7 +205,7 @@ var MHNavigationBar = React.createClass({
       return null;
     }
 
-    var initialStage = index === navStatePresentedIndex(this.props.navState) ?
+    let initialStage = index === navStatePresentedIndex(this.props.navState) ?
       this.props.navigationStyles.Stages.Center :
       this.props.navigationStyles.Stages.Left;
     rendered = (
@@ -223,7 +221,7 @@ var MHNavigationBar = React.createClass({
 
     this._descriptors[componentName] = this._descriptors[componentName].set(route, rendered);
     return rendered;
-  },
+  }
 
 });
 
@@ -234,8 +232,8 @@ var styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
-    backgroundColor: 'transparent',
-  },
+    backgroundColor: 'transparent'
+  }
 });
 
 module.exports = MHNavigationBar;
