a2f1079ccbcb57ef9bf143c20729deb646c21d9c
miot-robot <miot-robot@xiaomi.com>
2023-08-22 14:22:24 +0800

    Update useDeviceIcon.js refer ${gitlabAfter}

---
+++ miot-sdk/hooks/useDeviceIcon.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { Device, DeviceEvent, Service } from "miot";
+export default function useDeviceIcon() {
+  const [iconURL, setIconURL] = useState(Device.iconURL);
+  useEffect(() => {
+    if (Device.extraObj?.split?.parentId) {
+      Service.smarthome
+        .getDeviceIcon({ subclass_id: 0 })
+        .then(({ data: { proxy_category_icon } }) =>
+          setIconURL(proxy_category_icon)
+        );
+    }
+    const listener = DeviceEvent.deviceIconChanged.addListener(() => {
+      if (!Device.extraObj?.split?.parentId) {
+        setIconURL(Device.iconURL);
+      }
+    });
+    return () => {
+      listener && listener.remove && listener.remove();
+    };
+  }, []);
+  return iconURL;
+}
