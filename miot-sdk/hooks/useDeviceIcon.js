c16c4d596e59c6ba138b6668a028cd01f5264b7d
miot-robot <miot-robot@xiaomi.com>
2023-08-23 16:36:26 +0800

    增加日志 refer ${gitlabAfter}

--- miot-sdk/hooks/useDeviceIcon.js
+++ miot-sdk/hooks/useDeviceIcon.js
@@ -3,15 +3,35 @@ import { Device, DeviceEvent, Service } from "miot";
 export default function useDeviceIcon() {
   const [iconURL, setIconURL] = useState(Device.iconURL);
   useEffect(() => {
+    Service.smarthome.reportLog(
+      Device.model,
+      `IconHook ${ JSON.stringify(Device.extraObj) } ${ Device.iconURL }`
+    );
     if (Device.extraObj?.split?.parentId) {
+      Service.smarthome.reportLog(
+        Device.model,
+        `IconHook ${ Device.extraObj.split.parentId }`
+      );
       Service.smarthome
         .getDeviceIcon({ subclass_id: 0 })
-        .then(({ data: { proxy_category_icon } }) =>
-          setIconURL(proxy_category_icon)
-        );
+        .then(({ data: { proxy_category_icon } }) => {
+          Service.smarthome.reportLog(
+            Device.model,
+            `IconHook ${ proxy_category_icon }`
+          );
+          setIconURL(proxy_category_icon);
+        });
     }
     const listener = DeviceEvent.deviceIconChanged.addListener(() => {
+      Service.smarthome.reportLog(
+        Device.model,
+        `IconHook ${ JSON.stringify(Device.extraObj) } ${ Device.iconURL }`
+      );
       if (!Device.extraObj?.split?.parentId) {
+        Service.smarthome.reportLog(
+          Device.model,
+          `IconHook ${ Device.extraObj.split.parentId }`
+        );
         setIconURL(Device.iconURL);
       }
     });
