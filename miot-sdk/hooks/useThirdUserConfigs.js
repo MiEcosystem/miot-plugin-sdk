3ece47a8f5dc7205e98bf4586d8ad90e555526d3
miot-robot <miot-robot@xiaomi.com>
2023-12-05 17:10:14 +0800

    [feat] IOT-44791  智能门锁添加getHomeDevice接口权限 refer ${gitlabAfter}

---
+++ miot-sdk/hooks/useThirdUserConfigs.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { Device, DeviceEvent, Service } from "miot";
+import { DeviceEventEmitter } from 'react-native';
+export default function useThirdUserConfigs(key, model = Device.model, eventEmitter) {
+  const [userConfigs, setUserConfigs] = useState({});
+  useEffect(() => {
+    Service.storage.getThirdUserConfigsForOneKey(model, key).then((res) => {
+      // console.log('getThirdUserConfigsForOneKey--res----8888', res);
+      setUserConfigs(res?.data ? JSON.parse(res?.data) : {});
+    }).catch((error) => {
+      console.log('getThirdUserConfigsForOneKey---/device/deviceinfo---error', error);
+    });
+    let listener = DeviceEventEmitter.addListener(eventEmitter, (value) => {
+      // console.log('DeviceEventEmitter--useThirdUserConfigs--value---通知-', value);
+      // console.log('DeviceEventEmitter--userConfigs--userConfigs---通知-', userConfigs);
+      setUserConfigs({
+        ...userConfigs,
+        ...value
+      });
+      // {"1":{"ai_desc":"","home_id":1,"id":1,"name":"","icon":"","room_id":1},"2":{"ai_desc":"","home_id":1,"id":2,"name":"","icon":"","room_id":1}}
+      // setSwitchInfo(value);
+    });
+    return () => {
+      listener && listener.remove && listener.remove();
+    };
+  }, []);
+  return userConfigs;
+}
