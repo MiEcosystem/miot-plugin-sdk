a571b9fbe0a7cc9ea9edebe3d27f4337a822ee7f
miot-robot <miot-robot@xiaomi.com>
2023-12-21 16:18:14 +0800

    feat:新增rl refer ${gitlabAfter}

--- projects/com.xiaomi.demo/Main/UIComponent/swiper/Phone/index.js
+++ projects/com.xiaomi.demo/Main/UIComponent/swiper/Phone/index.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
   View,
   Image,
   StatusBar,
   Dimensions
-} from 'react-native'
-import Swiper from 'react-native-swiper'
-const { width, height } = Dimensions.get('window')
+} from 'react-native';
+import Swiper from 'react-native-swiper';
+const { width, height } = Dimensions.get('window');
 
 const styles = {
   wrapper: {
@@ -18,7 +18,7 @@ const styles = {
     backgroundColor: 'transparent'
   },
   container: {
-    flex: 1,
+    flex: 1
   },
 
   imgBackground: {
@@ -30,22 +30,22 @@ const styles = {
 
   image: {
     width,
-    height,
+    height
   }
-}
+};
 
 export default class extends Component {
-  render () {
+  render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle='light-content' />
+        <StatusBar barStyle="light-content" />
         <Image
           source={require('./img/bg.jpg')}
           style={styles.imgBackground}
         />
         <Swiper style={styles.wrapper}
-          dot={<View style={{backgroundColor: 'rgba(255,255,255,.3)', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
-          activeDot={<View style={{backgroundColor: '#fff', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
+          dot={<View style={{ backgroundColor: 'rgba(255,255,255,.3)', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7 }} />}
+          activeDot={<View style={{ backgroundColor: '#fff', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7 }} />}
           paginationStyle={{
             bottom: 70
           }}
@@ -54,14 +54,14 @@ export default class extends Component {
             <Image
               style={styles.image}
               source={require('./img/1.jpg')}
-              resizeMode='cover'
+              resizeMode="cover"
             />
           </View>
           <View style={styles.slide}>
             <Image
               style={styles.image}
               source={require('./img/2.jpg')}
-              resizeMode='cover'
+              resizeMode="cover"
             />
           </View>
           <View style={styles.slide}>
@@ -69,6 +69,6 @@ export default class extends Component {
           </View>
         </Swiper>
       </View>
-    )
+    );
   }
 }
