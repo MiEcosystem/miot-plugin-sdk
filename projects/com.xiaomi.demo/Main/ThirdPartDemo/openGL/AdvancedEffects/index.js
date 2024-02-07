b2b1575184333a7eb1e7b41fcc0c095184ad9cde
miot-robot <miot-robot@xiaomi.com>
2024-02-07 11:31:19 +0800

    [Mod] add camera api refer ${gitlabAfter}

--- projects/com.xiaomi.demo/Main/ThirdPartDemo/openGL/AdvancedEffects/index.js
+++ projects/com.xiaomi.demo/Main/ThirdPartDemo/openGL/AdvancedEffects/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {StyleSheet, View} from "react-native";
-import {resolveAssetSource} from "gl-react-native";
+import { StyleSheet, View } from "react-native";
+import { resolveAssetSource } from "gl-react-native";
 const { width: viewportW, height: viewportH } = require("Dimensions").get("window");
 
 import Banner from "./Banner";
@@ -11,43 +11,43 @@ import Slideshow from "./Slideshow";
 
 class AdvancedEffects extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       time: 0.02,
       frames: 1,
       embeddedImage: resolveAssetSource(require("./Image.jpg")),
       images:
-       //"MQtLWbD,N8a9CkZ,adCmISK,AedZQ4N,y9qRJR3,brzKTYZ,NSyk07l,EaZiWfn,I1KZdnl,DoQBdzT,slIt2Ww,DA12puU,IYLdRFW,oqmO4Po,T6NaLyI,6XAPrAY,thYzbif,4qmqo3o,8xT2J96,ZCa2pWq,loQfDN2,oabfA68,uOXqDRY,MyyS4vK,fhNYTX4"
+      // "MQtLWbD,N8a9CkZ,adCmISK,AedZQ4N,y9qRJR3,brzKTYZ,NSyk07l,EaZiWfn,I1KZdnl,DoQBdzT,slIt2Ww,DA12puU,IYLdRFW,oqmO4Po,T6NaLyI,6XAPrAY,thYzbif,4qmqo3o,8xT2J96,ZCa2pWq,loQfDN2,oabfA68,uOXqDRY,MyyS4vK,fhNYTX4"
         "wxqlQkh,G2Whuq3,0bUSEBX,giP58XN,iKdXwVm,IvpoR40,zJIxPEo,CKlmtPs,fnMylHI,vGXYiYy,MnOB9Le,YqsZKgc,0BJobQo,Otbz312"
           .split(",")
-          .map(id => `https://imgur.com/${id}.jpg`)
+          .map((id) => `https://imgur.com/${ id }.jpg`)
     };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     let startTime;
-    const loop = t => {
+    const loop = (t) => {
       this._raf = requestAnimationFrame(loop);
       if (!startTime) startTime = t;
       const time = (t - startTime) / 1000;
-      this.setState({ time: time, frames: this.state.frames+1 });
+      this.setState({ time: time, frames: this.state.frames + 1 });
     };
     this._raf = requestAnimationFrame(loop);
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     cancelAnimationFrame(this._raf);
   }
 
-  render () {
-    const {time, frames, images, embeddedImage} = this.state;
+  render() {
+    const { time, frames, images, embeddedImage } = this.state;
 
     const nbVignettes = 1;
-    const imgW = Math.floor(viewportW/nbVignettes);
-    const imgH = Math.floor((2/3)*viewportW/nbVignettes);
+    const imgW = Math.floor(viewportW / nbVignettes);
+    const imgH = Math.floor((2 / 3) * viewportW / nbVignettes);
     const introH = 100;
-    const transitionH = Math.floor((2/3)*viewportW);
+    const transitionH = Math.floor((2 / 3) * viewportW);
 
     return (
       <View style={styles.root}>
@@ -58,7 +58,7 @@ class AdvancedEffects extends React.Component {
         />
         <Intro
           time={time}
-          fps={frames/time}
+          fps={frames / time}
           width={viewportW}
           height={introH}
         />
