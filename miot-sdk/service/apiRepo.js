9072c1e437e7f00927bf6bbb7ff674705de27d6e
miot-robot <miot-robot@xiaomi.com>
2024-02-18 17:15:53 +0800

    修改path路径 refer ${gitlabAfter}

--- miot-sdk/service/apiRepo.js
+++ miot-sdk/service/apiRepo.js
@@ -319,7 +319,7 @@ const API_REPO = {
   '/common/app/get/event/statistics': {},
   
   // 趣拍回忆相关
-  '/common/app/nineSeconds/m3u8': {},
+  '/miot/camera/app/nineSeconds/m3u8': {},
   '/common/app/v1/dailyStory/generateMemory': {},
   '/common/app/get/emotion/status': {},
   '/common/app/get/event/emostatiscs': {},
