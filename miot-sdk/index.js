493ab6c9ae02071565dc55ede2f2d2640d4079af
miot-robot <miot-robot@xiaomi.com>
2024-10-12 17:11:39 +0800

    feat: SDK增加所有设备列表接口 refer ${gitlabAfter}

--- miot-sdk/index.js
+++ miot-sdk/index.js
@@ -29,14 +29,15 @@ import { AccelerometerChangeEvent as AccelerometerChangeEventNames } from './sys
 import { CompassChangeEvent as CompassChangeEventNames } from './system/compass';
 import { VolumeChangeEvent as VolumeChangeEventNames } from './system/volume';
 import { GyroscopeChangeEvent as GyroscopeChangeEventNames } from './system/gyroscope';
-import PackageInstance, { Entrance as Entrances, PackageEvent as PackageEventNames } from './Package';
-import { PrivacyEvent as PrivacyEventNames, CLOUD_PRIVACY_EVENT_TYPE , UserExpPlanEvent as UserExpPlanEventNames, USER_EXP_PLAN_EVENT_TYPE} from './utils/protocol-helper';
+import PackageInstance, { Entrance as Entrances, PackageEvent as PackageEventNames, PackageRoot } from './Package';
+import { PrivacyEvent as PrivacyEventNames, CLOUD_PRIVACY_EVENT_TYPE, UserExpPlanEvent as UserExpPlanEventNames, USER_EXP_PLAN_EVENT_TYPE } from './utils/protocol-helper';
 import { RootDeviceProperties } from "./Properties";
 import ResourcesPack from './resources';
 import ServiceInstance from './Service';
 import { SceneType as SceneTypeNames } from './service/scene';
 import DarkModeFactory from './darkmode';
-export const API_LEVEL = 10068
+import ReuseHelper from './utils/reuse-helper';
+export const API_LEVEL = 10100
 /**
  * 插件包基本配置
  * {@link module:miot/Package}
