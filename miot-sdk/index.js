08049838e466dfa4ef8cf5dee6a1b9b8df2cfd51
miot-robot <miot-robot@xiaomi.com>
2025-01-13 15:51:32 +0800

    Merge branch lipeng7-SDK_10104-patch-84326 into SDK_10104 refer ${gitlabAfter}

--- miot-sdk/index.js
+++ miot-sdk/index.js
@@ -37,7 +37,7 @@ import ServiceInstance from './Service';
 import { SceneType as SceneTypeNames } from './service/scene';
 import DarkModeFactory from './darkmode';
 import ReuseHelper from './utils/reuse-helper';
-export const API_LEVEL = 10103
+export const API_LEVEL = 10104
 /**
  * 插件包基本配置
  * {@link module:miot/Package}
