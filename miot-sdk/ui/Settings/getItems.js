87c059e6da404361f9c9d153a7595615a1a164ac
miot-robot <miot-robot@xiaomi.com>
2025-06-03 16:04:08 +0800

    [Task]为C700增加显示模式切换p2p_cmd指令； refer ${gitlabAfter}

--- miot-sdk/ui/Settings/getItems.js
+++ miot-sdk/ui/Settings/getItems.js
@@ -1,21 +1,48 @@
-import React, { isValidElement } from 'react';
+import React, { isValidElement, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Device } from 'miot';
+import Device from '../../device/BasicDevice';
 import ListItem from '../ListItem/ListItem';
 import useModelType from '../../hooks/useModelType';
+import useDeviceRoomInfo from '../../hooks/useDeviceRoomInfo';
+import tryTrackCommonSetting from "../../utils/track-sdk";
+import { useEffect } from 'react';
 export const clickedItems = [];
+export function useClicked(key) {
+  const [clicked, setClicked] = useState(clickedItems.includes(key));
+  function click() {
+    if (!clickedItems.includes(key)) {
+      clickedItems.push(key);
+    }
+    setClicked(true);
+  }
+  return [clicked, click];
+}
 export default function getItems(innerOptions, keys, values, params, defaultOptions) {
   const {
     options,
     showDots = [],
-    extraOptions: {
-      excludeOptions = []
-    }
+    extraOptions
   } = params;
   const { type, isOwner } = Device;
+  const { permitLevel } = useDeviceRoomInfo();
+  const isHomeManager = permitLevel === 9;
+  const { excludeRequiredOptions = [] } = extraOptions || {};
   const modelType = useModelType();
   // 最终配置的项，包括调用方传入的项，和默认项
   const mergedOptions = [...(new Set([...(options || []), ...(defaultOptions || [])]))];
+  const [createGroupResult, setCreateGroupResult] = useState(null);
+  useEffect(() => {
+    const fetchCreateGroupData = async() => {
+      try {
+        const result = await Device.isBelongToCarRoom(Device.deviceID);
+        setCreateGroupResult(result);
+      } catch (error) {
+        console.error('获取 createGroup 数据时出错:', error);
+      }
+    };
+    // 只在组件第一次挂载时执行一次
+    fetchCreateGroupData();
+  }, []);
   return keys.map((key, index) => {
     if (key instanceof Function) {
       return key(params);
@@ -23,15 +50,22 @@ export default function getItems(innerOptions, keys, values, params, defaultOpti
     if (isValidElement(key)) {
       return key;
     }
+    if (key instanceof Object && key.title) {
+      return (
+        <ListItem key={key.title} {...key } useNewType={true} />
+      );
+    }
     const option = innerOptions[key];
     if (!option) {
       return null;
     }
     const {
       Component,
+      needValue,
       types,
       notTypes,
       ownerOnly,
+      homeManagerAllowed,
       modelTypes,
       notModelTypes,
       validator,
@@ -43,7 +77,7 @@ export default function getItems(innerOptions, keys, values, params, defaultOpti
       // 未配置
       (!mergedOptions.includes(key)) ||
       // 指定排除
-      excludeOptions.includes(key) ||
+      excludeRequiredOptions.includes(key) ||
       // 设备类型匹配
       (types && !types.includes(type)) ||
       // 设备类型排除
@@ -53,39 +87,78 @@ export default function getItems(innerOptions, keys, values, params, defaultOpti
       // model 类型排除
       (notModelTypes && notModelTypes.includes(modelType)) ||
       // 不可共享
-      (!isOwner && ownerOnly) ||
+      (ownerOnly && !(isOwner || isHomeManager && homeManagerAllowed)) ||
+      // 无值
+      (needValue && [undefined, null, ''].includes(value)) ||
       // 自定义判断
       (validator instanceof Function && !validator({ modelType }))
     ) {
       return null;
     }
     if (Component instanceof Function) {
-      return Component(params);
+      // return Component(params);
+      tryTrackCommonSetting(String(key), 'expose');
+      return (
+        <Component key={String(key)} {...params} />
+      );
     }
     if (!title) {
       return null;
     }
-    return (
-      <ListItem
-        key={String(key)}
-        title={title instanceof Function ? title({
-          modelType
-        }) : title}
-        value={String([undefined, null].includes(value) ? '' : value)}
-        onPress={delegatePress(onPress, params, key)}
-        showDot={showDots.includes(key) && !clickedItems.includes(key)}
-        useNewType={true}
-        hideArrow={!onPress}
-      />
-    );
+    tryTrackCommonSetting(String(key), 'expose');
+    if (key === 'createGroup') {
+      // 车机隐藏创建灯组：code:0 表示成功，data:true表示车机，data:false表示非车机
+      if (createGroupResult && createGroupResult.code === 0 && createGroupResult.data === true) {
+        return null;
+      } else {
+        return (
+          <ListItem
+            key={String(key)}
+            title={title instanceof Function ? title({
+              modelType
+            }) : title}
+            value={String([undefined, null].includes(value) ? '' : value)}
+            onPress={delegatePress(onPress, params, key)}
+            showDot={showDots.includes(key) && !clickedItems.includes(key)}
+            useNewType={true}
+            hideArrow={!onPress}
+          />
+        );
+      }
+    } else {
+      return (
+        <ListItem
+          key={String(key)}
+          title={title instanceof Function ? title({
+            modelType
+          }) : title}
+          value={String([undefined, null].includes(value) ? '' : value)}
+          onPress={delegatePress(onPress, params, key)}
+          showDot={showDots.includes(key) && !clickedItems.includes(key)}
+          useNewType={true}
+          hideArrow={!onPress}
+        />
+      );
+    }
   });
 }
-export function delegatePress(cb, params, key) {
+export function delegatePress(cb, params, key, click) {
   return () => {
+    tryTrackCommonSetting(String(key), 'click');
+    const preOperation = params?.extraOptions?.preOperations?.[key];
     if (key && !clickedItems.includes(key)) {
       clickedItems.push(key);
+      if (click instanceof Function) {
+        click(key);
+      }
     }
     if (cb instanceof Function) {
+      if (preOperation instanceof Function) {
+        preOperation().then(() => {
+          cb(params || {});
+        }).catch(() => {});
+        return;
+      }
       cb(params || {});
     }
   };
