ea97a6f0dad46a8a896004b6e56878f467b257df
miot-robot <miot-robot@xiaomi.com>
2025-07-04 11:07:21 +0800

    feat 增加打开微信小程序接口 refer ${gitlabAfter}

--- miot-sdk/index.js
+++ miot-sdk/index.js
@@ -37,7 +37,7 @@ import ServiceInstance from './Service';
 import { SceneType as SceneTypeNames } from './service/scene';
 import DarkModeFactory from './darkmode';
 import ReuseHelper from './utils/reuse-helper';
-export const API_LEVEL = 10108
+export const API_LEVEL = 10109
 /**
  * 插件包基本配置
  * {@link module:miot/Package}
