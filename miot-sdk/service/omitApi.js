316b656b2b1754641bd7f9a0508e32ab169690c5
miot-robot <miot-robot@xiaomi.com>
2025-09-06 17:29:15 +0800

    BUGOS2-616326:解决固件升级页面，升级按钮与小白条轻微重叠问题 refer ${gitlabAfter}

--- miot-sdk/service/omitApi.js
+++ miot-sdk/service/omitApi.js
@@ -1,2 +1,2 @@
-const OMIT_API = ['/appgateway/third/miwifi/app'];
+const OMIT_API = ['/appgateway/third/miwifi/app', '/eco', '/business/outplatform', '/hlthproc/inner/hlthcenter'];
 export default OMIT_API;
